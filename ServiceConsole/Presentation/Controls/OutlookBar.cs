63fb8fcf002c1c38981b113163bdcfc70190e93d
kerryjiang <none@none.com>
2010-06-27 07:12:47 +0000

    Import code of service console

---
+++ ServiceConsole/Presentation/Controls/OutlookBar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.ServiceConsole.Presentation.Controls
+{
+    class OutlookBar
+    {
+    }
+}
