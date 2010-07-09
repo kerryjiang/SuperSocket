4f93fb7a1f582afe155da7407730b9d275f69ee1
kerryjiang <none@none.com>
2010-07-09 16:32:58 +0000

    1) Update VS 2010 project file 2) Change assembly version

--- SocketServiceCore/GlobalAssemblyInfo.cs
+++ SocketServiceCore/GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.5.0")]
-[assembly: AssemblyFileVersion("0.1.5.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
