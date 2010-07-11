a6165e1fb6892c023bd3ae4584e736fd499f24c5
kerryjiang <none@none.com>
2010-07-11 05:24:27 +0000

    Update assembly version to 1.0

--- SocketServiceCore/GlobalAssemblyInfo.cs
+++ SocketServiceCore/GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
