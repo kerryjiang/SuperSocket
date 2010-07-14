31db70c1a9d7d7403dd0783375108101fa329844
kerryjiang <none@none.com>
2010-07-14 15:23:56 +0000

    Implement the two receive data method for async socket

--- SocketServiceCore/AsyncSocketSession.cs
+++ SocketServiceCore/AsyncSocketSession.cs
@@ -229,14 +229,135 @@ namespace SuperSocket.SocketServiceCore
             //TODO: Implement async socket SSL/TLS encryption
         }
 
+        /// <summary>
+        /// Receives the data.
+        /// Process data synchronously, because command execution is waiting the received data
+        /// </summary>
+        /// <param name="storeSteram">The store steram.</param>
+        /// <param name="length">The length.</param>
         public override void ReceiveData(Stream storeSteram, int length)
         {
-            throw new NotImplementedException();
+            byte[] buffer = this.SocketAsyncProxy.SocketEventArgs.Buffer;
+
+            int thisRead = 0;
+            int leftRead = length;
+            int shouldRead = 0;
+
+            while (leftRead > 0)
+            {
+                shouldRead = Math.Min(buffer.Length, leftRead);
+                thisRead = Client.Receive(buffer, 0, shouldRead, SocketFlags.None);
+
+                if (thisRead <= 0)
+                {
+                    //Slow speed? Wait a moment
+                    Thread.Sleep(100);
+                    continue;
+                }
+
+                storeSteram.Write(buffer, 0, thisRead);
+                leftRead -= thisRead;
+            }
         }
 
+        /// <summary>
+        /// Receives the data.
+        /// Process data synchronously, because command execution is waiting the received data
+        /// </summary>
+        /// <param name="storeSteram">The store steram.</param>
+        /// <param name="endMark">The end mark.</param>
         public override void ReceiveData(Stream storeSteram, byte[] endMark)
         {
-            throw new NotImplementedException();
+            byte[] buffer = this.SocketAsyncProxy.SocketEventArgs.Buffer;
+            byte[] lastData = new byte[endMark.Length];
+            int lastDataSzie = 0;
+
+            int thisRead = 0;
+
+            while (true)
+            {
+                thisRead = Client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+                if (thisRead > 0)
+                {
+                    if (thisRead >= endMark.Length)
+                    {
+                        if (EndsWith(buffer, 0, thisRead, endMark))
+                        {
+                            storeSteram.Write(buffer, 0, thisRead);
+                            return;
+                        }
+                        else
+                        {
+                            storeSteram.Write(buffer, 0, thisRead);
+                            Array.Copy(buffer, thisRead - endMark.Length - 1, lastData, 0, endMark.Length);
+                            lastDataSzie = endMark.Length;
+                        }
+                    }
+                    else
+                    {
+                        bool matched = false;
+
+                        int searchIndex = endMark.Length - 1;
+
+                        for (int i = thisRead - 1; i >= 0 && searchIndex >= 0; i--, searchIndex--)
+                        {
+                            if (endMark[searchIndex] != buffer[i])
+                            {
+                                matched = false;
+                                break;
+                            }
+                            else
+                            {
+                                matched = true;
+                            }
+                        }
+
+                        if (lastDataSzie > 0)
+                        {
+                            for (int i = lastDataSzie - 1; i >= 0 && searchIndex >= 0; i--, searchIndex--)
+                            {
+                                if (endMark[searchIndex] != lastData[i])
+                                {
+                                    matched = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    matched = true;
+                                }
+                            }
+                        }
+
+                        if (matched && searchIndex < 0)
+                        {
+                            storeSteram.Write(buffer, 0, thisRead);
+                            return;
+                        }
+                        else
+                        {
+                            storeSteram.Write(buffer, 0, thisRead);
+
+                            if (lastDataSzie + thisRead <= lastData.Length)
+                            {
+                                Array.Copy(buffer, 0, lastData, lastDataSzie, thisRead);
+                                lastDataSzie = lastDataSzie + thisRead;
+                            }
+                            else
+                            {
+                                Array.Copy(lastData, thisRead + lastDataSzie - lastData.Length, lastData, 0, lastData.Length - thisRead);
+                                Array.Copy(buffer, 0, lastData, lastDataSzie, thisRead);
+                                lastDataSzie = endMark.Length;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                    continue;
+                }
+            }
         }
     }
 }
