617123e2f587a9015b68acf4670c18b4c77fc536
kerryjiang <none@none.com>
2010-07-25 16:12:59 +0000

    Added UnitTest project

---
+++ Test/Command/ECHO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Command;
+
+namespace SuperSocket.Test.Command
+{
+    class ECHO : CommandBase<TestSession>
+    {
+        protected override void Execute(TestSession session, CommandInfo commandData)
+        {
+            session.SendResponse(commandData.Param);
+        }
+    }
+}
