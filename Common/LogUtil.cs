627e9c4f5059457e1794c4b5ad7f2e9bdaea8b0d
kerryjiang <none@none.com>
2010-07-25 16:12:59 +0000

    Added UnitTest project

--- Common/LogUtil.cs
+++ Common/LogUtil.cs
@@ -6,7 +6,7 @@ namespace SuperSocket.Common
 {
     public class LogUtil
     {
-        private static ILogger m_logger = new EventLogger();
+        private static ILogger m_logger = null;
 
         public static void Setup(ILogger logger)
         {
@@ -15,52 +15,62 @@ namespace SuperSocket.Common
 
         public static void LogError(Exception e)
         {
-            m_logger.LogError(e);
+            if (m_logger != null)
+                m_logger.LogError(e);
         }
 
         public static void LogError(ILogApp app, Exception e)
         {
-            m_logger.LogError(app, e);
+            if (m_logger != null)
+                m_logger.LogError(app, e);
         }
 
         public static void LogError(string title, Exception e)
         {
-            m_logger.LogError(title, e);
+            if (m_logger != null)
+                m_logger.LogError(title, e);
         }
 
         public static void LogError(ILogApp app, string title, Exception e)
         {
-            m_logger.LogError(app, title, e);
+            if (m_logger != null)
+                m_logger.LogError(app, title, e);
         }
 
         public static void LogError(string message)
         {
-            m_logger.LogError(message);
+            if (m_logger != null)
+                m_logger.LogError(message);
         }
 
         public static void LogError(ILogApp app, string message)
         {
-            m_logger.LogError(app, message);
+            if (m_logger != null)
+                m_logger.LogError(app, message);
         }
 
         public static void LogDebug(string message)
         {
-            m_logger.LogDebug(message);
+            if (m_logger != null)
+                m_logger.LogDebug(message);
         }
 
         public static void LogDebug(ILogApp app, string message)
         {
-            m_logger.LogDebug(app, message);
+            if (m_logger != null)
+                m_logger.LogDebug(app, message);
         }
 
         public static void LogInfo(string message)
         {
-            m_logger.LogInfo(message);
+            if (m_logger != null)
+                m_logger.LogInfo(message);
         }
 
         public static void LogInfo(ILogApp app, string message)
         {
-            m_logger.LogInfo(app, message);
+            if (m_logger != null)
+                m_logger.LogInfo(app, message);
         }
     }
 }
