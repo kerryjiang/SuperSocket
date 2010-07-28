deb7df4e5c2d4d189812adea1c7ef75fa3558cb8
kerryjiang <none@none.com>
2010-07-28 01:12:40 +0000

    Committed missed files in Test project

---
+++ Test/Command/RECEL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Command;
+using System.Resources;
+using System.Reflection;
+using System.IO;
+
+namespace SuperSocket.Test.Command
+{
+    public class RECEL : CommandBase<TestSession>
+    {
+        protected override void Execute(TestSession session, CommandInfo commandData)
+        {
+            int length = int.Parse(commandData[0]);
+
+            MemoryStream ms = new MemoryStream();
+            session.SocketSession.ReceiveData(ms, length);
+            byte[] data = ms.ToArray();
+            session.SocketSession.GetUnderlyStream().Write(data, 0, data.Length);
+            session.SocketSession.GetUnderlyStream().Flush();
+        }
+    }
+}
