813b4827edd0af20a08b2d25f850ebeb4a9b096f
kerryjiang <none@none.com>
2010-07-29 13:02:30 +0000

    Added a new quick start sample "CustomCommandParser"

---
+++ QuickStart/CustomCommandParser/CustomCommandParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Command;
+
+namespace SuperSocket.QuickStart.CustomCommandParser
+{
+    /// <summary>
+    /// CMD:ECHO AabSfght5656D5Cfa5==
+    /// </summary>
+    public class CustomCommandParser : ICommandParser
+    {
+        #region ICommandParser Members
+
+        public CommandInfo ParseCommand(string command)
+        {
+            if(!command.StartsWith("CMD:"))
+                return null;
+
+            command = command.Substring(4);
+            string[] data = command.Split(' ');
+            return new CommandInfo(data[0], data[1]);
+        }
+
+        #endregion
+    }
+}
