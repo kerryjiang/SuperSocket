2c5e7e9bc1e711f491de093488ef3aac56de3c2d
kerryjiang <none@none.com>
2010-07-29 13:02:30 +0000

    Added a new quick start sample "CustomCommandParser"

---
+++ QuickStart/CustomCommandParser/YourServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore;
+
+namespace SuperSocket.QuickStart.CustomCommandParser
+{
+    public class YourServer : AppServer<YourSession>
+    {
+        public YourServer() : base()
+        {
+            this.CommandParser = new CustomCommandParser();
+            this.CommandParameterParser = new Base64CommandParameterParser();
+        }
+    }
+}
