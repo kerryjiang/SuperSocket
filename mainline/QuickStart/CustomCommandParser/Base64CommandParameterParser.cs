64122354175b2fd067b10804107eeefcf1b5bba9
kerryjiang <none@none.com>
2010-10-04 06:50:49 +0000

    Checked in source code of mainline

---
+++ mainline/QuickStart/CustomCommandParser/Base64CommandParameterParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Command;
+
+namespace SuperSocket.QuickStart.CustomCommandParser
+{
+    class Base64CommandParameterParser : ICommandParameterParser
+    {
+        #region ICommandParameterParser Members
+
+        public string[] ParseCommandParameter(string parameter)
+        {
+            return Encoding.ASCII.GetString(Convert.FromBase64String(parameter)).Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        #endregion
+    }
+}
