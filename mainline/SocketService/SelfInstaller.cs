8d7e3143567bc41c62ffeb41c5bd18a0f38c337a
kerryjiang <none@none.com>
2010-10-04 06:50:49 +0000

    Checked in source code of mainline

---
+++ mainline/SocketService/SelfInstaller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SuperSocket.SocketService
+{
+    public static class SelfInstaller
+    {
+        private static readonly string _exePath = Assembly.GetExecutingAssembly().Location;
+
+        public static bool InstallMe()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { _exePath });
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool UninstallMe()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { "/u", _exePath });
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
