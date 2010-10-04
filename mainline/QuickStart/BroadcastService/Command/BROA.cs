8ac2377528f26c8d6f6268f7bc0384c458b2e478
kerryjiang <none@none.com>
2010-10-04 06:50:49 +0000

    Checked in source code of mainline

---
+++ mainline/QuickStart/BroadcastService/Command/BROA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Command;
+
+namespace SuperSocket.QuickStart.BroadcastService.Command
+{
+    public class BROA : CommandBase<BroadcastSession>
+    {
+        protected override void Execute(BroadcastSession session, CommandInfo commandData)
+        {
+            string message = commandData.Param;
+            session.AppServer.BroadcastMessage(session, message);
+            session.SendResponse("101 message broadcasted");
+        }
+    }
+}
