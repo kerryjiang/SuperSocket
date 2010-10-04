6018a88e241fdf4265531907638d6a47845835e7
kerryjiang <none@none.com>
2010-10-04 06:50:49 +0000

    Checked in source code of mainline

---
+++ mainline/Test/SyncSocketServerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SuperSocket.SocketServiceCore;
+using SuperSocket.SocketServiceCore.Config;
+
+namespace SuperSocket.Test
+{
+    [TestFixture]
+    public class SyncSocketServerTest : SocketServerTest
+    {
+        public SyncSocketServerTest()
+            : base(new ServerConfig
+            {
+                Ip = "Any",
+                LogCommand = true,
+                MaxConnectionNumber = 1,
+                Mode = SocketMode.Sync,
+                Name = "Sync Test Socket Server",
+                Port = 100
+            })
+
+        {
+ 
+        }
+    }
+}
