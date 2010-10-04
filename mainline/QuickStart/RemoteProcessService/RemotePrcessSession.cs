ecf6148be1cc8590e1ef13e9e7df7b6eae7c0c84
kerryjiang <none@none.com>
2010-10-04 06:50:49 +0000

    Checked in source code of mainline

---
+++ mainline/QuickStart/RemoteProcessService/RemotePrcessSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore;
+
+namespace SuperSocket.QuickStart.RemoteProcessService
+{
+    public class RemotePrcessSession : AppSession<RemotePrcessSession, RemoteProcessServer>
+    {
+        protected override void OnClosed()
+        {
+            
+        }
+
+        public override void SayWelcome()
+        {
+            SendResponse("Welcome to use this tool!");
+        }
+
+        public override void HandleExceptionalError(Exception e)
+        {
+            SendResponse("An error has occurred in server side! Error message: " + e.Message + "!");
+        }
+    }
+}
