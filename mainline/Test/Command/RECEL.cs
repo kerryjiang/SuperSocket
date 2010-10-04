a7d2cb58c2e1c1a50875398136ff2b2bfa00965c
kerryjiang <none@none.com>
2010-10-04 06:50:49 +0000

    Checked in source code of mainline

---
+++ mainline/Test/Command/RECEL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Command;
+using System.Resources;
+using System.Reflection;
+using System.IO;
+
+namespace SuperSocket.Test.Command
+{
+    public class RECEL : CommandBase<TestSession>
+    {
+        protected override void Execute(TestSession session, CommandInfo commandData)
+        {
+            int length = int.Parse(commandData[0]);
+
+            MemoryStream ms = new MemoryStream();
+            session.SocketSession.ReceiveData(ms, length);
+            byte[] data = ms.ToArray();
+            session.SocketSession.SendResponse(session.AppContext, data);
+        }
+    }
+}
