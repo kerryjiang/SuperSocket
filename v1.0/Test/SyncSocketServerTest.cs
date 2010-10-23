c29bfa1d2f632341613bb88c15f2bcfe946dc56d
kerryjiang <none@none.com>
2010-10-23 12:52:10 +0000

    Create branch for v 1.0

---
+++ v1.0/Test/SyncSocketServerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SuperSocket.SocketServiceCore;
+using SuperSocket.SocketServiceCore.Config;
+
+namespace SuperSocket.Test
+{
+    [TestFixture]
+    public class SyncSocketServerTest : SocketServerTest
+    {
+        protected override IServerConfig DefaultServerConfig
+        {
+            get
+            {
+                return new ServerConfig
+                    {
+                        Ip = "Any",
+                        LogCommand = true,
+                        MaxConnectionNumber = 3,
+                        Mode = SocketMode.Sync,
+                        Name = "Sync Test Socket Server",
+                        Port = 100
+                    };
+            }
+        }
+    }
+}
---
+++ v1.0/Test/TestServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore;
+using SuperSocket.SocketServiceCore.Command;
+
+namespace SuperSocket.Test
+{
+    public class TestServer : AppServer<TestSession>
+    {
+        public TestServer()
+        {
+
+        }
+
+        public TestServer(ICommandParser commandParser)
+            : this(commandParser, null)
+        {
+
+        }
+
+        public TestServer(ICommandParser commandParser, ICommandParameterParser commandParameterParser)
+            : this()
+        {
+            CommandParser = commandParser;
+            CommandParameterParser = commandParameterParser;
+        }
+    }
+}
