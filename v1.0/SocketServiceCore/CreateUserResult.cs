4030edc6a396bc5676a022491c1ce83ac13064a3
kerryjiang <none@none.com>
2010-10-23 12:52:10 +0000

    Create branch for v 1.0

---
+++ v1.0/SocketServiceCore/CreateUserResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace SuperSocket.SocketServiceCore
+{
+    [DataContract]
+    public enum CreateUserResult
+    {
+        [EnumMember]
+        UserNameAlreadyExist,
+
+        [EnumMember]
+        Success,
+
+        [EnumMember]
+        UnknownError
+    }
+}
