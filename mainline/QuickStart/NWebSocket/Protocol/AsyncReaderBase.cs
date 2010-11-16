f138584f0447bcc9d698ac3a708e328dfd28fe56
kerryjiang <none@none.com>
2010-11-16 14:55:30 +0000

    mainline: added a project "NWebSocket" in QuickStart to show how to implement custom protocol

---
+++ mainline/QuickStart/NWebSocket/Protocol/AsyncReaderBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Protocol;
+using SuperSocket.Common;
+
+namespace NWebSocket.Protocol
+{
+    public abstract class AsyncReaderBase : ICommandAsyncReader
+    {
+        protected ArraySegmentList<byte> Segments { get; set; }
+
+        #region ICommandAsyncReader Members
+
+        public abstract bool FindCommand(byte[] readBuffer, int offset, int length, out byte[] commandData);
+
+        public ArraySegmentList<byte> GetLeftBuffer()
+        {
+            return Segments;
+        }
+
+        public ICommandAsyncReader NextCommandReader { get; protected set; }
+
+        #endregion
+    }
+}
---
+++ mainline/QuickStart/NWebSocket/Protocol/DataAsyncReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketServiceCore.Protocol;
+using SuperSocket.Common;
+
+namespace NWebSocket.Protocol
+{
+    public class DataAsyncReader : AsyncReaderBase
+    {
+        public DataAsyncReader(HeaderAsyncReader headerReader)
+        {
+            Segments = headerReader.GetLeftBuffer();
+        }
+
+        #region ICommandAsyncReader Members
+
+        public override bool FindCommand(byte[] readBuffer, int offset, int length, out byte[] commandData)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
