ee846c0af3c19bd337a05ac3f8a962270a32687b
kerryjiang <none@none.com>
2010-11-21 13:21:51 +0000

    mainline: refactored almost most of the code to support custom protocol

---
+++ mainline/SocketEngine/Configuration/CredentialConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Config;
+
+namespace SuperSocket.SocketEngine.Configuration
+{
+    public class CredentialConfig : ConfigurationElement, ICredentialConfig
+    {
+        #region ICredentialConfig Members
+
+        [ConfigurationProperty("userName", IsRequired = true)]
+        public string UserName
+        {
+            get { return this["userName"] as string; }
+        }
+
+        [ConfigurationProperty("password", IsRequired = true)]
+        public string Password
+        {
+            get { return this["password"] as string; }
+        }
+
+        #endregion
+    }
+}
