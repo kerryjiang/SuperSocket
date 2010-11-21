81473f347e5fee1f0c54c5053099ce91b7d77b72
kerryjiang <none@none.com>
2010-11-21 13:05:22 +0000

    mainline: refactored almost most of the code to support custom protocol

---
+++ mainline/SocketBase/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperSocket.SocketBase")]
+[assembly: AssemblyDescription("SuperSocket.SocketBase")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("427204a0-fa48-4c22-97d6-4daf08ee4354")]
