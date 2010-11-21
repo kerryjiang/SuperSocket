d2e3934ebba461a9de0fa0ca2648d6253038e3d1
kerryjiang <none@none.com>
2010-11-21 13:12:08 +0000

    mainline: refactored almost most of the code to support custom protocol

--- mainline/Common/Properties/AssemblyInfo.cs
+++ mainline/Common/Properties/AssemblyInfo.cs
@@ -5,15 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SuperSocket.Common")]
+[assembly: AssemblyDescription("SuperSocket.Common")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("kerry")]
-[assembly: AssemblyProduct("Common")]
-[assembly: AssemblyCopyright("Copyright © kerry 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
