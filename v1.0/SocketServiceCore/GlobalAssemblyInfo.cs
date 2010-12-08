a087a2fc7c5a9c45ff465dd8f347b9f9853de821
kerryjiang <none@none.com>
2010-12-08 04:46:37 +0000

    v1.0: updated assembly version number for 1.0

--- v1.0/SocketServiceCore/GlobalAssemblyInfo.cs
+++ v1.0/SocketServiceCore/GlobalAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
