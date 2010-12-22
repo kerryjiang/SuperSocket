1ade924bbc92ccbe0f49b221c68e7771c1c8dba2
kerryjiang <none@none.com>
2010-12-22 16:05:03 +0000

    mainline: changed to use log4net, support IPv6

---
+++ mainline/Common/Log4NetLogger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using log4net.Core;
+
+namespace SuperSocket.Common
+{
+    public class Log4NetLogger : LoggerBase
+    {
+        protected ILog Logger { get; private set; }
+
+        private string m_ServerTemplate = "{0}:";
+        private string m_ServerDetailTemplate = "{0} : {1}";
+
+        public Log4NetLogger()
+            : this("SuperSocket")
+        {
+
+        }
+
+        public Log4NetLogger(string name)
+            : this(name, null)
+        {
+            
+        }
+
+        public Log4NetLogger(string name, ILogger nestedLogger)
+            : base(name, nestedLogger)
+        {
+            Logger = LogManager.GetLogger(name);
+        }
+
+        #region ILogger Members
+
+        public override void LogError(Exception e)
+        {
+            Logger.Error(string.Format(m_ServerTemplate, Name), e);
+            base.LogError(e);
+        }
+
+        public override void LogError(string title, Exception e)
+        {
+            Logger.Error(string.Format(m_ServerDetailTemplate, Name, title), e);
+            base.LogError(title, e);
+        }
+
+        public override void LogError(string message)
+        {
+            Logger.ErrorFormat(m_ServerDetailTemplate, Name, message);
+            base.LogError(message);
+        }
+
+        public override void LogDebug(string message)
+        {
+            Logger.DebugFormat(m_ServerDetailTemplate, Name, message);
+            base.LogDebug(message);
+        }
+
+        public override void LogInfo(string message)
+        {
+            Logger.InfoFormat(m_ServerDetailTemplate, Name, message);
+            base.LogInfo(message);
+        }
+
+        public override void LogPerf(string message)
+        {
+            Logger.Logger.Log(new LoggingEvent(new LoggingEventData
+                {
+                    LoggerName = Name,
+                    Message = message,
+                    Level = Level.Trace
+                }));
+            base.LogPerf(message);
+        }
+
+        #endregion
+    }
+}
---
+++ mainline/SocketBase/Config/RootConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.SocketBase.Config
+{
+    public class RootConfig : IRootConfig
+    {
+        #region IRootConfig Members
+
+        public ICredentialConfig CredentialConfig { get; set; }
+
+        public string ConsoleBaseAddress { get; set; }
+
+        public LoggingMode LoggingMode { get; set; }
+
+        [Obsolete]
+        public bool IndependentLogger { get; set; }
+
+        #endregion
+    }
+}
