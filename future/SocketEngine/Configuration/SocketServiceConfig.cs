c5b1ad0dac22393487ffd42c0895b4b1aede2b78
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/SocketEngine/Configuration/SocketServiceConfig.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.SocketEngine.Configuration
+{
+    public class SocketServiceConfig : ConfigurationSection, IConfig
+    {
+
+        [ConfigurationProperty("servers")]
+        public ServerCollection Servers
+        {
+            get
+            {
+                return this["servers"] as ServerCollection;
+            }
+        }
+
+        [ConfigurationProperty("services")]
+        public ServiceCollection Services
+        {
+            get
+            {
+                return this["services"] as ServiceCollection;
+            }
+        }
+
+        [ConfigurationProperty("credential", IsRequired = false)]
+        public CredentialConfig Credential
+        {
+            get
+            {
+                return this["credential"] as CredentialConfig;
+            }
+        }
+
+        [ConfigurationProperty("consoleBaseAddress", IsRequired = false)]
+        public string ConsoleBaseAddress
+        {
+            get
+            {
+                return this["consoleBaseAddress"] as string;
+            }
+        }
+
+        [Obsolete]
+        [ConfigurationProperty("independentLogger", IsRequired = false, DefaultValue = false)]
+        public bool IndependentLogger
+        {
+            get
+            {
+                return (bool)this["independentLogger"];
+            }
+        }
+
+        [ConfigurationProperty("loggingMode", IsRequired = false, DefaultValue = "ShareFile")]
+        public LoggingMode LoggingMode
+        {
+            get
+            {
+                return (LoggingMode)this["loggingMode"];
+            }
+        }
+
+        #region IConfig Members
+
+        public List<IServerConfig> GetServerList()
+        {
+            List<IServerConfig> serverList = new List<IServerConfig>();
+
+            foreach (Server server in Servers)
+            {
+                serverList.Add(server);
+            }
+
+            return serverList;
+        }
+
+        public List<IServiceConfig> GetServiceList()
+        {
+            List<IServiceConfig> serviceList = new List<IServiceConfig>();
+
+            foreach (Service service in Services)
+            {
+                serviceList.Add(service);
+            }
+
+            return serviceList;
+        }
+
+        public ICredentialConfig CredentialConfig
+        {
+            get { return Credential; }
+        }
+
+        #endregion
+    }
+}
