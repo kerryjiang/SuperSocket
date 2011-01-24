aa8e1bcf768db1124ec1e1d7a484dff08555b56e
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/Test/Command/NUM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.Test.Command
+{
+    public class NUM : StringCommandBase<TestSession>
+    {
+        public const string ReplyFormat = "325 received {0}!";
+
+        public override void ExecuteCommand(TestSession session, StringCommandInfo commandData)
+        {
+            session.SendResponse(string.Format(ReplyFormat, commandData.Data));
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "325";
+            }
+        }
+    }
+}
