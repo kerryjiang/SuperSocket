dfc901c2290c4422560e8e97ab32f96ba40c7bf4
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/Common/StringExtension.NET35.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Common
+{
+    public static partial class StringExtension
+    {
+        public static bool TryParseEnum<T>(this string value, bool ignoreCase, out T enumValue)
+            where T : struct
+        {
+            try
+            {
+                enumValue = (T)System.Enum.Parse(typeof(T), value, ignoreCase);
+                return true;
+            }
+            catch
+            {
+                enumValue = default(T);
+                return false;
+            }
+        }
+    }
+}
---
+++ future/Common/StringExtension.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Common
+{
+    public static partial class StringExtension
+    {
+        public static int ToInt32(this string source)
+        {
+            return source.ToInt32(0);
+        }
+
+        public static int ToInt32(this string source, int defaultValue)
+        {
+            if (string.IsNullOrEmpty(source))
+                return defaultValue;
+
+            int value;
+
+            if (!int.TryParse(source, out value))
+                value = defaultValue;
+
+            return value;
+        }
+
+        public static long ToLong(this string source)
+        {
+            return source.ToLong(0);
+        }
+
+        public static long ToLong(this string source, long defaultValue)
+        {
+            if (string.IsNullOrEmpty(source))
+                return defaultValue;
+
+            long value;
+
+            if (!long.TryParse(source, out value))
+                value = defaultValue;
+
+            return value;
+        }
+
+        public static short ToShort(this string source)
+        {
+            return source.ToShort(0);
+        }
+
+        public static short ToShort(this string source, short defaultValue)
+        {
+            if (string.IsNullOrEmpty(source))
+                return defaultValue;
+
+            short value;
+
+            if (!short.TryParse(source, out value))
+                value = defaultValue;
+
+            return value;
+        }
+
+        public static decimal ToDecimal(this string source)
+        {
+            return source.ToDecimal(0);
+        }
+
+        public static decimal ToDecimal(this string source, decimal defaultValue)
+        {
+            if (string.IsNullOrEmpty(source))
+                return defaultValue;
+
+            decimal value;
+
+            if (!decimal.TryParse(source, out value))
+                value = defaultValue;
+
+            return value;
+        }
+
+        public static DateTime ToDateTime(this string source)
+        {
+            return source.ToDateTime(DateTime.MinValue);
+        }
+
+        public static DateTime ToDateTime(this string source, DateTime defaultValue)
+        {
+            if (string.IsNullOrEmpty(source))
+                return defaultValue;
+
+            DateTime value;
+
+            if (!DateTime.TryParse(source, out value))
+                value = defaultValue;
+
+            return value;
+        }
+
+        public static bool ToBoolean(this string source)
+        {
+            return source.ToBoolean(false);
+        }
+
+        public static bool ToBoolean(this string source, bool defaultValue)
+        {
+            if (string.IsNullOrEmpty(source))
+                return defaultValue;
+
+            bool value;
+
+            if (!bool.TryParse(source, out value))
+                value = defaultValue;
+
+            return value;
+        }
+
+        
+    }
+}
