63f73d6c51e571e0971e031fd735101b0c353293
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/QuickStart/CustomCommandParser/YourServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.CustomCommandParser
+{
+    public class YourServer : AppServer<YourSession>
+    {
+        public YourServer() : base()
+        {
+            Protocol = new CommandLineProtocol(new CustomCommandParser());
+        }
+    }
+}
