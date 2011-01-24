3fd5758e219f2894e66a25421899f48bd7863aa8
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/QuickStart/BroadcastService/Command/CONN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.BroadcastService.Command
+{
+    public class CONN : StringCommandBase<BroadcastSession>
+    {
+        public override void ExecuteCommand(BroadcastSession session, StringCommandInfo commandData)
+        {
+            session.DeviceNumber = commandData[0];
+            session.AppServer.RegisterNewSession(session);
+            session.SendResponse("100 Connected");
+        }
+    }
+}
