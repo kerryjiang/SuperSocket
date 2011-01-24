cd6f621fe89acfd312ea92297c523fa8e2455d4d
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/QuickStart/CustomCommandParser/Command/ECHO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.CustomCommandParser.Command
+{
+    public class ECHO : StringCommandBase<YourSession>
+    {
+        public override void ExecuteCommand(YourSession session, StringCommandInfo commandData)
+        {
+            foreach (var p in commandData.Parameters)
+            {
+                session.SendResponse(p);
+            }
+        }
+    }
+}
