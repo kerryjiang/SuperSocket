0ab54a18374db0558ccfecb435bd683c5f9be59a
kerryjiang <none@none.com>
2011-01-24 06:30:09 +0000

    create a branch for future

---
+++ future/QuickStart/CustomProtocol/Command/ECHO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.CustomProtocol.Command
+{
+    public class ECHO : CommandBase<CustomProtocolSession, BinaryCommandInfo>
+    {
+        public override void ExecuteCommand(CustomProtocolSession session, BinaryCommandInfo commandInfo)
+        {
+            session.SendResponse(Encoding.ASCII.GetString(commandInfo.Data) + Environment.NewLine);
+        }
+    }
+}
