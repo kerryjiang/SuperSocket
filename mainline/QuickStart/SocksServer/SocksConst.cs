72840f575bac17a2819b0b612ddcce306e3b424c
kerryjiang <none@none.com>
2011-02-25 16:00:00 +0000

    mainline: tried to implement SocksServer

---
+++ mainline/QuickStart/SocksServer/SocksConst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.QuickStart.SocksServer
+{
+    class SocksConst
+    {
+        public const string CONN = "CONN";
+
+        public const string DATA = "DATA";
+    }
+}
---
+++ mainline/QuickStart/SocksServer/SocksProtocol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.SocksServer
+{
+    class SocksProtocol : ICustomProtocol<BinaryCommandInfo>
+    {
+        #region ICustomProtocol<BinaryCommandInfo> Members
+
+        public ICommandReader<BinaryCommandInfo> CreateCommandReader(IAppServer appServer)
+        {
+            return new ConnectCommandReader(appServer);
+        }
+
+        #endregion
+    }
+}
