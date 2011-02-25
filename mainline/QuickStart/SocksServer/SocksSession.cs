1ff10a3a875e6798ea2ff1b4f205fb7f975ac642
kerryjiang <none@none.com>
2011-02-25 09:53:56 +0000

    mainline: added new sample project "SocksServer" in QuickStart

---
+++ mainline/QuickStart/SocksServer/SocksSession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.SocksServer
+{
+    public class SocksSession : AppSession<SocksSession>
+    {
+    }
+}
