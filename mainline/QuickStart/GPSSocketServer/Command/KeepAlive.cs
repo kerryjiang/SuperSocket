2c7c3f47914461079d072fde68befd1d8fdcb033
kerryjiang <none@none.com>
2011-04-29 14:33:45 +0000

    mainline: added another CustomProtocol sample project GPSSocketServer

---
+++ mainline/QuickStart/GPSSocketServer/Command/KeepAlive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.GPSSocketServer.Command
+{
+    public class KeepAlive : CommandBase<GPSSession, BinaryCommandInfo>
+    {
+        public override string Name
+        {
+            get
+            {
+                return "10";
+            }
+        }
+
+        public override void ExecuteCommand(GPSSession session, BinaryCommandInfo commandInfo)
+        {
+            //You can do nothing, after this command is executed, the LastActiveTime of this session will be updated
+            session.SendResponse(session.AppServer.DefaultResponse);
+        }
+    }
+}
