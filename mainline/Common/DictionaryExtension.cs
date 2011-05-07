1143948eaf13dd2c52ae6e2c06b6af194fa36972
kerryjiang <none@none.com>
2011-05-07 13:30:42 +0000

    mainline: added DictionaryExtension class in SuperSocket.Common project

---
+++ mainline/Common/DictionaryExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Common
+{
+    public static class DictionaryExtension
+    {
+        public static T GetValue<T>(this IDictionary<object, object> dictionary, object key)
+            where T : new()
+        {
+            T defaultValue = default(T);
+            return GetValue<T>(dictionary, key, defaultValue);
+        }
+
+        public static T GetValue<T>(this IDictionary<object, object> dictionary, object key, T defaultValue)
+        {
+            object valueObj;
+
+            if (!dictionary.TryGetValue(key, out valueObj))
+            {
+                return defaultValue;
+            }
+            else
+            {
+                return (T)valueObj;
+            }
+        }
+    }
+}
