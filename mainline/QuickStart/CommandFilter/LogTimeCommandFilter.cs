837fea650e5897f63d7162c3cf41665d4d6852d1
kerryjiang <none@none.com>
2011-05-11 14:26:24 +0000

    mainline: added a new QuickStart project CommandFilter

---
+++ mainline/QuickStart/CommandFilter/LogTimeCommandFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.CommandFilter
+{
+    public class LogTimeCommandFilter : CommandFilterAttribute
+    {
+        public override void OnCommandExecuting(IAppSession session, ICommand command)
+        {
+            session.Items["StartTime"] = DateTime.Now;
+        }
+
+        public override void OnCommandExecuted(IAppSession session, ICommand command)
+        {
+            var startTime = session.Items.GetValue<DateTime>("StartTime");
+            var ts = DateTime.Now.Subtract(startTime);
+
+            if (ts.TotalSeconds > 5)
+            {
+                session.Logger.LogPerf(string.Format("A command '{0}' took {1} seconds!", command.Name, ts.ToString()));
+            }
+        }
+    }
+}
