0ee150e0bec1c775e530366353e4434973afabcc
kerryjiang <none@none.com>
2011-05-18 16:18:30 +0000

    Tools: improved performance test tool

---
+++ Tools/PerformanceTestServer/TestServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
+using SuperSocket.SocketBase.Command;
+
+namespace PerformanceTestServer
+{
+    public class TestServer : AppServer<TestSession>
+    {
+        public override bool Setup(IRootConfig rootConfig, IServerConfig config, ISocketServerFactory socketServerFactory, ICustomProtocol<StringCommandInfo> protocol)
+        {
+            if (!base.Setup(rootConfig, config, socketServerFactory, protocol))
+                return false;
+
+            string sendResponse = config.Options.GetValue("sendResponse", "false");
+            
+            bool sendResponseValue = false;
+            bool.TryParse(sendResponse, out sendResponseValue);
+
+            SendResponseToClient = sendResponseValue;
+            return true;
+        }
+
+        internal bool SendResponseToClient { get; private set; }
+    }
+}
