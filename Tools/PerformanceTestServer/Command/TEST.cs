d6f768f2dbb27b51bfad1a68d2a1285c378e645b
kerryjiang <none@none.com>
2011-05-18 16:18:30 +0000

    Tools: improved performance test tool

---
+++ Tools/PerformanceTestServer/Command/TEST.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+
+namespace PerformanceTestServer.Command
+{
+    public class TEST : StringCommandBase<TestSession>
+    {
+        public override void ExecuteCommand(TestSession session, StringCommandInfo commandInfo)
+        {
+            if (session.AppServer.SendResponseToClient)
+            {
+                session.SendResponse(commandInfo.Data);
+            }
+        }
+    }
+}
