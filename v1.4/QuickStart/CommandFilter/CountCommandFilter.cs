c656027d44e64b51b3c7bab748a3b326800459d2
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/CommandFilter/CountCommandFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using System.Threading;
+
+namespace SuperSocket.QuickStart.CommandFilter
+{
+    public class CountCommandFilter : CommandFilterAttribute
+    {
+        private long m_Total = 0;
+
+        public override void OnCommandExecuting(IAppSession session, ICommand command)
+        {
+
+        }
+
+        public override void OnCommandExecuted(IAppSession session, ICommand command)
+        {
+            Interlocked.Increment(ref m_Total);
+        }
+
+        public long Total
+        {
+            get { return m_Total; }
+        }
+    }
+}
