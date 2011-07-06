ad68fc545a715e2a9b73c55f3d26261b5634f3a6
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/EchoService/EchoSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.EchoService
+{
+    public class EchoSession : AppSession<EchoSession>
+    {
+        public override void StartSession()
+        {
+            SendResponse("Welcome to EchoServer!");
+        }
+
+        public override void HandleExceptionalError(Exception e)
+        {
+            SendResponse("Server side error occurred!");
+        }
+    }
+}
