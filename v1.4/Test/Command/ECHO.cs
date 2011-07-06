37f1116ddf6fc97415190894f996b01b7632cdbd
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/Test/Command/ECHO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.Test.Command
+{
+    public class ECHO : StringCommandBase<TestSession>
+    {
+        public override void ExecuteCommand(TestSession session, StringCommandInfo commandData)
+        {
+            Console.WriteLine("R:" + commandData.Data);
+            session.SendResponse(commandData.Data);
+        }
+    }
+}
---
+++ v1.4/Test/Command/PARA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.Test.Command
+{
+    public class PARA : StringCommandBase<TestSession>
+    {
+        public override void ExecuteCommand(TestSession session, StringCommandInfo commandData)
+        {
+            foreach (var p in commandData.Parameters)
+            {
+                Console.WriteLine("S: " + p);
+                session.SendResponse(p);
+            }
+        }
+    }
+}
