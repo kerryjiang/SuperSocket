4c8da40176b487f9a80770c948de7fdfebaa35bf
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/RemoteProcessService/RemoteProcessSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.RemoteProcessService
+{
+    public class RemoteProcessSession : AppSession<RemoteProcessSession>
+    {
+        public new RemoteProcessServer AppServer
+        {
+            get { return (RemoteProcessServer)base.AppServer; }
+        }
+
+        public override void StartSession()
+        {
+            SendResponse("Welcome to use this tool!");
+        }
+
+        public override void HandleExceptionalError(Exception e)
+        {
+            SendResponse("An error has occurred in server side! Error message: " + e.Message + "!");
+        }
+    }
+}
