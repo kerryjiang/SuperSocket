ae57064630f1049b6dbaa1432aeadd0ef4d3ed07
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/GPSSocketServer/Command/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.GPSSocketServer.Command
+{
+    public class Position : CommandBase<GPSSession, BinaryCommandInfo>
+    {
+        public override string Name
+        {
+            get
+            {
+                return "1A";
+            }
+        }
+
+        public override void ExecuteCommand(GPSSession session, BinaryCommandInfo commandInfo)
+        {
+            //The logic of saving GPS position data
+            session.SendResponse(session.AppServer.DefaultResponse);
+        }
+    }
+}
