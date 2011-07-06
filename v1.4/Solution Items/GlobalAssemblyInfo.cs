efe551b5cda3ecee440ac8d2dac83a7bd4d764c6
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/Solution Items/GlobalAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyCompany("SuperSocket")]
+[assembly: AssemblyProduct("SuperSocket")]
+[assembly: AssemblyInformationalVersion("1.4.0.0")]
+[assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
