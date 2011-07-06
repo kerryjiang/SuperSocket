b05bff2fc444a86eb50be71771ad2bfa262da0a8
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/Common/SocketEx.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net.Sockets;
+
+namespace SuperSocket.Common
+{
+    public static class SocketEx
+    {
+        public static void SafeCloseClientSocket(this Socket client, ILogger logger)
+        {
+            if(client == null)
+                return;
+
+            if (!client.Connected)
+                return;
+            
+            try
+            {
+                client.Shutdown(SocketShutdown.Both);
+            }
+            catch(ObjectDisposedException)
+            {
+            }
+            catch(Exception e)
+            {
+                if(logger != null)
+                    logger.LogError(e);
+            }
+            
+            try
+            {
+                client.Close();
+            }
+            catch(ObjectDisposedException)
+            {
+            }
+            catch(Exception e)
+            {
+                if(logger != null)
+                    logger.LogError(e);
+            }
+        }
+        
+        public static void SafeCloseClientSocket(this Socket client)
+        {
+            //No logger
+            client.SafeCloseClientSocket(null);
+        }
+    }
+}
+
---
+++ v1.4/Common/StringExtension.NET4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Common
+{
+    public static partial class StringExtension
+    {
+        public static bool TryParseEnum<T>(this string value, bool ignoreCase, out T enumValue)
+            where T : struct
+        {
+            return Enum.TryParse<T>(value, ignoreCase, out enumValue);
+        }
+    }
+}
