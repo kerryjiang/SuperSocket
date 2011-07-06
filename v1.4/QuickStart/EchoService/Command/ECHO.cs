9f455c9fa936ff2945e5dbff63b569859901e830
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/EchoService/Command/ECHO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.EchoService.Command
+{
+    public class ECHO : StringCommandBase<EchoSession>
+    {
+        #region CommandBase<EchoSession> Members
+
+        public override void ExecuteCommand(EchoSession session, StringCommandInfo commandInfo)
+        {
+            session.SendResponse(commandInfo.Data);
+        }
+
+        #endregion
+    }
+}
