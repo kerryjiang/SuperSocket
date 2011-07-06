e7080b8acff23e9606fd789db0667083d2105e32
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/SocketEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperSocket.SocketEngine")]
+[assembly: AssemblyDescription("SuperSocket.SocketEngine")]
+[assembly: AssemblyConfiguration("")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a9e989e2-6cd5-461c-a171-0eacec4e9c0a")]
