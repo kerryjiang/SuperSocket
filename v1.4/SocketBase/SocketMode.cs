8bb7eb5092e14133f95663a7251bd5d736b03c54
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/SocketBase/SocketMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.SocketBase
+{
+    public enum SocketMode
+    {
+        Sync,
+        Async,
+        Udp
+    }
+}
