8fe41e340f20d4a05f779ef5c4f4d8024a3fd329
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/Test/Common/AssemblyUtilTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.Common;
+
+namespace SuperSocket.Test.Common
+{
+    [TestFixture]
+    public class AssemblyUtilTest
+    {
+        [Test]
+        public void TestCopyProperties()
+        {
+            var config = new ServerConfig();
+            config.Name = "Kerry";
+            config.Port = 21;
+            var newConfig = new ServerConfig();
+            config.CopyPropertiesTo(newConfig);
+
+            Assert.AreEqual(config.Name, newConfig.Name);
+            Assert.AreEqual(config.Port, newConfig.Port);
+        }
+    }
+}
