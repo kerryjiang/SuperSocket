3932c3044bd130811a6d7740acaba3418ab3d166
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/BroadcastService/Command/BROA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.QuickStart.BroadcastService.Command
+{
+    public class BROA : StringCommandBase<BroadcastSession>
+    {
+        public override void ExecuteCommand(BroadcastSession session, StringCommandInfo commandData)
+        {
+            string message = commandData.Data;
+            session.AppServer.BroadcastMessage(session, message);
+            session.SendResponse("101 message broadcasted");
+        }
+    }
+}
