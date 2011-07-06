944049acd34d4ad037d6944eb053fa688e5214be
kerryjiang <none@none.com>
2011-07-06 13:36:14 +0000

    create branch for v1.4

---
+++ v1.4/QuickStart/MultipleAppServer/MyAppServerA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
+
+namespace SuperSocket.QuickStart.MultipleAppServer
+{
+    public class MyAppServerA : AppServer
+    {
+        private IDespatchServer m_DespatchServer;
+
+        protected override void OnStartup()
+        {
+            m_DespatchServer = SocketServerManager.GetServerByName("ServerB") as IDespatchServer;
+            base.OnStartup();
+        }
+
+        internal void DespatchMessage(string targetSessionKey, string message)
+        {
+            m_DespatchServer.DispatchMessage(targetSessionKey, message);
+        }
+    }
+}
