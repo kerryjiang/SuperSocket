9e1aaf090e5dc36ae567a230656cd2027e4b2157
kerryjiang <none@none.com>
2011-07-12 14:40:45 +0000

    mainline: tried to implement dynamic command loader

---
+++ mainline/Dlr/DynamicCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Scripting.Hosting;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.Dlr
+{
+    class DynamicCommand<TAppSession, TCommandInfo> : ICommand<TAppSession, TCommandInfo>
+        where TAppSession : IAppSession, IAppSession<TAppSession, TCommandInfo>, new()
+        where TCommandInfo : ICommandInfo
+    {
+        private static ScriptRuntime m_ScriptRuntime;
+
+        private Action<TAppSession, TCommandInfo> m_DynamicExecuteCommand;
+
+        static DynamicCommand()
+        {
+            m_ScriptRuntime = ScriptRuntime.CreateFromConfiguration();
+        }
+
+        public DynamicCommand(string filePath)
+        {
+            FilePath = filePath;
+            Name = Path.GetFileNameWithoutExtension(filePath);
+            var scriptEngine = m_ScriptRuntime.GetEngineByFileExtension(Path.GetExtension(filePath));
+            var scriptScode = scriptEngine.GetScope(filePath);
+
+            Action<TAppSession, TCommandInfo> dynamicMethod;
+            if (scriptScode != null || !scriptScode.TryGetVariable<Action<TAppSession, TCommandInfo>>("ExecuteCommand", out dynamicMethod))
+                throw new Exception("Failed to find a command execution method in file: " + filePath);
+
+            m_DynamicExecuteCommand = dynamicMethod;
+        }
+
+        #region ICommand<TAppSession,TCommandInfo> Members
+
+        public virtual void ExecuteCommand(TAppSession session, TCommandInfo commandInfo)
+        {
+            m_DynamicExecuteCommand(session, commandInfo);
+        }
+
+        #endregion
+
+        public string FilePath { get; private set; }
+
+        #region ICommand Members
+
+        public string Name { get; private set; }
+
+        #endregion
+    }
+}
---
+++ mainline/Dlr/DynamicCommandLoader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.Dlr
+{
+    public class DynamicCommandLoader : ICommandLoader
+    {
+        public IEnumerable<ICommand<TAppSession, TCommandInfo>> LoadCommands<TAppSession, TCommandInfo>(IAppServer appServer)
+            where TAppSession : IAppSession, IAppSession<TAppSession, TCommandInfo>, new()
+            where TCommandInfo : ICommandInfo
+        {
+            var commandDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Command");
+            var serverCommandDir = Path.Combine(commandDir, appServer.Name);
+
+            if (!Directory.Exists(commandDir))
+                return null;
+
+            List<string> commandFiles = new List<string>();
+
+            commandFiles.AddRange(Directory.GetFiles(commandDir, "*.py", SearchOption.TopDirectoryOnly));
+
+            if (Directory.Exists(serverCommandDir))
+            {
+                commandFiles.AddRange(Directory.GetFiles(serverCommandDir, "*.py", SearchOption.TopDirectoryOnly));
+            }
+
+            if (!commandFiles.Any())
+                return null;
+
+            var commands = new List<ICommand<TAppSession, TCommandInfo>>(commandFiles.Count);
+
+            foreach (var file in commandFiles)
+            {
+                DynamicCommand<TAppSession, TCommandInfo> command;
+
+                try
+                {
+                    command = new DynamicCommand<TAppSession, TCommandInfo>(file);
+                    commands.Add(command);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Failed to load command file: " + file + "!", e);
+                }
+            }
+
+            return commands;
+        }
+    }
+}
