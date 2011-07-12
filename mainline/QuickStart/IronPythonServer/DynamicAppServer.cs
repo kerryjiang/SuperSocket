16d57bc865ad539380f153a1b3b3dd8be2650267
kerryjiang <none@none.com>
2011-07-12 15:46:23 +0000

    mainline: added QuickStart project IronPythonServer to demo new dynamic command support

---
+++ mainline/QuickStart/IronPythonServer/DynamicAppServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.IronPythonServer
+{
+    public class DynamicAppServer : AppServer
+    {
+    }
+}
