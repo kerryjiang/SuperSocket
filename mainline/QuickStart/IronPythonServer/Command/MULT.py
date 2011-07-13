898ce384500cf2947eff357aa39136a120cf2ef5
kerryjiang <none@none.com>
2011-07-13 09:36:58 +0000

    mainline: improved QuickStart project IronPythonServer

--- mainline/QuickStart/IronPythonServer/Command/MULT.py
+++ mainline/QuickStart/IronPythonServer/Command/MULT.py
@@ -1,9 +1,11 @@
 ﻿import clr
+clr.AddReference("System")
 clr.AddReference("SuperSocket.Common")
 clr.AddReference("SuperSocket.SocketBase")
 
+from System import *
 from SuperSocket.Common import *
 from SuperSocket.SocketBase import *
 
 def execute(session, command):
-	session.SendResponse(command.Data)
+	session.SendResponse((Convert.ToInt32(command[0]) * Convert.ToInt32(command[1])).ToString())
