699dbd0106f50c9c5637ceaca1c79a5b3134416e
kerryjiang <none@none.com>
2011-07-14 15:18:35 +0000

    mainline: improved IronSocketServer quickstart project

--- mainline/QuickStart/IronPythonServer/Command/MULT.py
+++
@@ -1,7 +0,0 @@
-﻿import clr
-clr.AddReference("System")
-
-from System import *
-
-def execute(session, command):
-	session.SendResponse((Convert.ToInt32(command[0]) * Convert.ToInt32(command[1])).ToString())
