9ff3f13b1ff2937b42d35d629a88a63cfa259538
kerryjiang <none@none.com>
2011-07-14 15:18:35 +0000

    mainline: improved IronSocketServer quickstart project

---
+++ mainline/QuickStart/IronSocketServer/DynamicAppServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.IronSocketServer
+{
+    public class DynamicAppServer : AppServer
+    {
+    }
+}
