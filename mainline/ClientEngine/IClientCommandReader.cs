b7e06a88b9dbafea87976c275aac1e70208c60a6
kerryjiang <none@none.com>
2011-07-28 14:06:12 +0000

    mainline: added new project SuperSocket.ClientEngine

---
+++ mainline/ClientEngine/IClientCommandReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+
+namespace SuperSocket.ClientEngine
+{
+    public interface IClientCommandReader<TCommandInfo>
+        where TCommandInfo : ICommandInfo
+    {
+        TCommandInfo GetCommandInfo(byte[] readBuffer, int offset, int length, out int left);
+    }
+}
