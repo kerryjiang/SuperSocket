bbf5753578fd481a5b42f7f89b910796300fad5b
kerryjiang <none@none.com>
2011-11-14 16:08:01 +0000

    mainline: tried to support TLS/SSL over Async SslStream

---
+++ mainline/SocketEngine/AsyncStreamSocketServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.SocketEngine
+{
+    class AsyncStreamSocketServer<TAppSession, TCommandInfo> : TcpSocketServerBase<AsyncStreamSocketSession<TAppSession, TCommandInfo>, TAppSession>
+        where TAppSession : IAppSession, IAppSession<TAppSession, TCommandInfo>, new()
+        where TCommandInfo : ICommandInfo
+    {
+        public AsyncStreamSocketServer(IAppServer<TAppSession> appServer, IPEndPoint localEndPoint, ICustomProtocol<TCommandInfo> protocol)
+            : base(appServer, localEndPoint)
+        {
+            
+        }
+    }
+}
---
+++ mainline/SocketEngine/AsyncStreamSocketSession.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using System.Net.Sockets;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.SocketEngine
+{
+    class AsyncStreamSocketSession<TAppSession, TCommandInfo> : SocketSession<TAppSession, TCommandInfo>, IAsyncSocketSession
+        where TAppSession : IAppSession, IAppSession<TAppSession, TCommandInfo>, new()
+        where TCommandInfo : ICommandInfo
+    {
+        public AsyncStreamSocketSession(Socket client, ICommandReader<TCommandInfo> initialCommandReader)
+            : base(client, initialCommandReader)
+        {
+            
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendResponse(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendResponse(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendResponse(byte[] data, int offset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ApplySecureProtocol()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReceiveData(System.IO.Stream storeSteram, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReceiveData(System.IO.Stream storeSteram, byte[] endMark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AsyncSocket.SocketAsyncEventArgsProxy SocketAsyncProxy
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void ProcessReceive(System.Net.Sockets.SocketAsyncEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Common.ILogger Logger
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
