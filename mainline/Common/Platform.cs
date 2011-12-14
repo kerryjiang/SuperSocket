dc56134bc258724a82a1ed0059af8a50b89bdd6c
kerryjiang <none@none.com>
2011-12-14 03:52:36 +0000

    mainline: removed solution file and project files for Mono, because Mono and .NET can share some files

---
+++ mainline/Common/Platform.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SuperSocket.Common
+{
+    /// <summary>
+    /// This class is designed for detect platform attribute in runtime
+    /// </summary>
+    public static class Platform
+    {
+        public static void Initialize()
+        {
+            try
+            {
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.IOControl(IOControlCode.KeepAliveValues, null, null);
+                SupportSocketIOControlByCodeEnum = true;
+            }
+            catch (NotSupportedException)
+            {
+                SupportSocketIOControlByCodeEnum = false;
+            }
+            catch (NotImplementedException)
+            {
+                SupportSocketIOControlByCodeEnum = false;
+            }
+            catch (Exception)
+            {
+                SupportSocketIOControlByCodeEnum = true;
+            }
+        }
+
+        public static bool SupportSocketIOControlByCodeEnum { get; private set; }
+    }
+}
