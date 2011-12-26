8e77d93feba4aecec032b96de43d16c60513e534
kerryjiang <none@none.com>
2011-12-26 16:49:12 +0000

    v1.4: 1) added strong name compiling support 2) added build batch script 3) updated revision number in GlobalAssemblyInfo.cs

---
+++ v1.4/Build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set fdir=%WINDIR%\Microsoft.NET\Framework64
+
+if not exist %fdir% (
+	set fdir=%WINDIR%\Microsoft.NET\Framework
+)
+
+set msbuild=%fdir%\v4.0.30319\msbuild.exe
+
+%msbuild% SuperSocket.2010.sln /p:Configuration=Release /t:Rebuild /p:keyfile=supersocket.snk
+
+%msbuild% SuperSocket.2010.NET35.sln /p:Configuration=Release /t:Rebuild /p:keyfile=supersocket.snk
+
+pause
