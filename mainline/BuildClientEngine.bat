79e67ce5076e54a1c2189e1f022ba306b21c9ee6
kerryjiang <none@none.com>
2012-01-07 10:16:33 +0000

    mainline: fixed bugs in client engine build script

--- mainline/BuildClientEngine.bat
+++ mainline/BuildClientEngine.bat
@@ -3,10 +3,31 @@
 set fdir=%WINDIR%\Microsoft.NET\Framework
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
-%msbuild% SuperSocket.ClientEngine.2010.sln /p:Configuration=Debug /t:Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+%msbuild% ClientEngine\SuperSocket.ClientEngine.csproj /p:Configuration=Debug /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
-%msbuild% SuperSocket.ClientEngine.2010.sln /p:Configuration=Release /t:Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+%msbuild% ClientEngine\SuperSocket.ClientEngine.csproj /p:Configuration=Release /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.Net35.csproj /p:Configuration=Debug /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.Net35.csproj /p:Configuration=Release /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.Mono.csproj /p:Configuration=Debug /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.Mono.csproj /p:Configuration=Release /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+set fdir=%WINDIR%\Microsoft.NET\Framework
+set msbuild=%fdir%\v4.0.30319\msbuild.exe
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.Silverlight.csproj /p:Configuration=Debug /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.Silverlight.csproj /p:Configuration=Release /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
 pause
