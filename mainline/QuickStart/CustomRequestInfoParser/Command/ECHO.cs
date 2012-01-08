8ac46d66d7f9a7c1b7e58a11d1cf0239ff374e96
kerryjiang <none@none.com>
2012-01-08 08:38:25 +0000

    mainline: changed CommandReader to RequestFilter

---
+++ mainline/QuickStart/CustomRequestInfoParser/Command/ECHO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.CustomCommandParser.Command
+{
+    public class ECHO : StringCommandBase<YourSession>
+    {
+        public override void ExecuteCommand(YourSession session, StringRequestInfo commandData)
+        {
+            foreach (var p in commandData.Parameters)
+            {
+                session.SendResponse(p);
+            }
+        }
+    }
+}
