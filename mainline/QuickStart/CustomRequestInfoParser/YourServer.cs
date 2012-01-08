f80ea610632b3374f01f9484cf196c666c0b97cd
kerryjiang <none@none.com>
2012-01-08 08:38:25 +0000

    mainline: changed CommandReader to RequestFilter

---
+++ mainline/QuickStart/CustomRequestInfoParser/YourServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.CustomCommandParser
+{
+    public class YourServer : AppServer<YourSession>
+    {
+        public YourServer()
+            : base(new CommandLineRequestFilterFactory(Encoding.Default, new CustomRequestInfoParser()))
+        {
+ 
+        }
+    }
+}
---
+++ mainline/QuickStart/CustomRequestInfoParser/YourSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.CustomCommandParser
+{
+    public class YourSession : AppSession<YourSession>
+    {
+        public override void StartSession()
+        {
+            SendResponse("Welcome");   
+        }
+
+        public override void HandleExceptionalError(Exception e)
+        {
+            
+        }
+    }
+}
