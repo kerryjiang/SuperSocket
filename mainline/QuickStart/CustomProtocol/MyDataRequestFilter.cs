79da297b1c064c40457efcc67e0e20ca1f52549a
kerryjiang <none@none.com>
2012-01-08 08:38:25 +0000

    mainline: changed CommandReader to RequestFilter

---
+++ mainline/QuickStart/CustomProtocol/MyDataRequestFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.CustomProtocol
+{
+    class MyDataRequestFilter : RequestFilterBase<BinaryRequestInfo>
+    {
+        private int m_Length;
+
+        private string m_CommandName;
+
+        internal void Initialize(string commandName, int length, RequestFilterBase<BinaryRequestInfo> previousCommandReader)
+        {
+            m_Length = length;
+            m_CommandName = commandName;
+
+            base.Initialize(previousCommandReader);
+        }
+
+        public override BinaryRequestInfo Filter(IAppSession<BinaryRequestInfo> session, byte[] readBuffer, int offset, int length, bool toBeCopied, out int left)
+        {
+            left = 0;
+
+            int leftLength = m_Length - BufferSegments.Count;
+
+            AddArraySegment(readBuffer, offset, length, toBeCopied);
+
+            if (length >= leftLength)
+            {
+                NextRequestFilter = new MyRequestFilter();
+                var requestInfo = new BinaryRequestInfo(m_CommandName, BufferSegments.ToArrayData(0, m_Length));
+
+                if (length > leftLength)
+                    left = length - leftLength;
+
+                return requestInfo;
+            }
+            else
+            {
+                NextRequestFilter = this;
+                return null;
+            }
+        }
+    }
+}
---
+++ mainline/QuickStart/CustomProtocol/MyRequestFilter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.CustomProtocol
+{
+    class MyRequestFilter : RequestFilterBase<BinaryRequestInfo>
+    {
+        private readonly MyDataRequestFilter m_PreparedDataReader = new MyDataRequestFilter();
+
+        private MyDataRequestFilter GetMyCommandDataReader(string commandName, int dataLength)
+        {
+            m_PreparedDataReader.Initialize(commandName, dataLength, this);
+            return m_PreparedDataReader;
+        }
+
+        public override BinaryRequestInfo Filter(IAppSession<BinaryRequestInfo> session, byte[] readBuffer, int offset, int length, bool toBeCopied, out int left)
+        {
+            left = 0;
+
+            int leftLength = 10 - this.BufferSegments.Count;
+
+            if (length < leftLength)
+            {
+                AddArraySegment(readBuffer, offset, length, toBeCopied);
+                NextRequestFilter = this;
+                return null;
+            }
+
+            AddArraySegment(readBuffer, offset, leftLength, toBeCopied);
+
+            string commandName = BufferSegments.Decode(Encoding.ASCII, 0, 4);
+            int commandDataLength = Convert.ToInt32(BufferSegments.Decode(Encoding.ASCII, 5, 4).TrimStart('0'));
+
+            if (length > leftLength)
+            {
+                int leftDataLength = length - leftLength;
+                if (leftDataLength >= commandDataLength)
+                {
+                    byte[] commandData = readBuffer.CloneRange(offset + leftLength, commandDataLength);
+                    BufferSegments.ClearSegements();
+                    NextRequestFilter = this;
+                    var requestInfo = new BinaryRequestInfo(commandName, commandData);
+
+                    //The next requestInfo is comming
+                    if (leftDataLength > commandDataLength)
+                        left = leftDataLength - commandDataLength;
+
+                    return requestInfo;
+                }
+                else// if (leftDataLength < commandDataLength)
+                {
+                    //Clear previous cached header data
+                    BufferSegments.ClearSegements();
+                    //Save left data part
+                    AddArraySegment(readBuffer, offset + leftLength, length - leftLength, toBeCopied);
+                    NextRequestFilter = GetMyCommandDataReader(commandName, commandDataLength);
+                    return null;
+                }
+            }
+            else
+            {
+                NextRequestFilter = GetMyCommandDataReader(commandName, commandDataLength);
+                return null;
+            }
+        }
+    }
+}
