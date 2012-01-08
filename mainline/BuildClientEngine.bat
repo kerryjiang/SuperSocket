6368172e612a142b0404b64d8ac10e5938062159
kerryjiang <none@none.com>
2012-01-08 13:04:43 +0000

    mainline: added WindowsPhone version for ClientEngine

--- mainline/BuildClientEngine.bat
+++ mainline/BuildClientEngine.bat
@@ -30,4 +30,10 @@ FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 %msbuild% ClientEngine\SuperSocket.ClientEngine.Silverlight.csproj /p:Configuration=Release /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
+%msbuild% ClientEngine\SuperSocket.ClientEngine.WindowsPhone.csproj /p:Configuration=Debug /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% ClientEngine\SuperSocket.ClientEngine.WindowsPhone.csproj /p:Configuration=Release /t:Clean;Rebuild /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
 pause
---
+++ mainline/ClientEngine/Properties/AssemblyInfo.WindowsPhone.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperSocket.ClientEngine for Windows Phone")]
+[assembly: AssemblyDescription("SuperSocket.ClientEngine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2D09D6BD-8A7B-4ADB-931E-064A8EE063F9")]
