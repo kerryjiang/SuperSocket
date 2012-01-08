088eaf52636f11582c1449142205acb83408113a
kerryjiang <none@none.com>
2012-01-08 08:38:25 +0000

    mainline: changed CommandReader to RequestFilter

---
+++ mainline/Facility/PolicyServer/FixSizeRequestFilter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+using SuperSocket.SocketBase;
+using SuperSocket.Common;
+
+namespace SuperSocket.Facility.PolicyServer
+{
+    public class FixSizeRequestFilter : IRequestFilter<BinaryRequestInfo>
+    {
+        private byte[] m_Buffer;
+        private int m_CurrentReceived = 0;
+
+        public FixSizeRequestFilter(int fixCommandSize)
+        {
+            m_Buffer = new byte[fixCommandSize];
+        }
+
+        public BinaryRequestInfo Filter(IAppSession<BinaryRequestInfo> session, byte[] readBuffer, int offset, int length, bool toBeCopied, out int left)
+        {
+            left = 0;
+
+            if (m_CurrentReceived + length <= m_Buffer.Length)
+            {
+                Array.Copy(readBuffer, offset, m_Buffer, m_CurrentReceived, length);
+                m_CurrentReceived += length;
+
+                if (m_CurrentReceived < m_Buffer.Length)
+                    return null;
+            }
+            else
+            {
+                Array.Copy(readBuffer, offset, m_Buffer, m_CurrentReceived, m_Buffer.Length - m_CurrentReceived);
+            }
+
+            m_CurrentReceived = 0;
+
+            return new BinaryRequestInfo("REQU", m_Buffer);
+        }
+
+        public int LeftBufferSize
+        {
+            get { return m_CurrentReceived; }
+        }
+
+        public IRequestFilter<BinaryRequestInfo> NextRequestFilter
+        {
+            get { return null; }
+        }
+    }
+}
---
+++ mainline/Facility/PolicyServer/FixSizeRequestFilterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.Facility.PolicyServer
+{
+    public class FixSizeRequestFilterFactory : IRequestFilterFactory<BinaryRequestInfo>
+    {
+        public int FixRequestSize { get; private set; }
+
+        public FixSizeRequestFilterFactory(int fixRequestSize)
+        {
+            FixRequestSize = fixRequestSize;
+        }
+
+        public IRequestFilter<BinaryRequestInfo> CreateFilter(IAppServer appServer)
+        {
+            return new FixSizeRequestFilter(FixRequestSize);
+        }
+    }
+}
