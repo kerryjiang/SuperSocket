0eb5cd7bb4d2bf49ee5406e27f5aa95878686642
kerryjiang <none@none.com>
2012-02-12 07:03:09 +0000

    mainline: improved client engine to support TLS/SSL

---
+++ mainline/ClientEngine/DataEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.ClientEngine
+{
+    public class DataEventArgs : EventArgs
+    {
+        public byte[] Data { get; set; }
+
+        public int Offset { get; set; }
+
+        public int Length { get; set; }
+    }
+}
