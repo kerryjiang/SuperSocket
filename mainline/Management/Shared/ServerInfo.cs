29644b6553d1bacd7a007db1ff61540f6df8b3cc
kerryjiang <none@none.com>
2012-02-29 14:27:31 +0000

    mainline: checked in initial projects framework of "SuperSocket Server Manager"

---
+++ mainline/Management/Shared/ServerInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Shared
+{
+    public class ServerInfo
+    {
+        public string Name { get; set; }
+
+        public bool IsRunning { get; set; }
+
+        public DateTime StartedTime { get; set; }
+
+        public TimeSpan RunningTime { get; set; }
+
+        public int MaxConnectionCount { get; set; }
+
+        public int CurrentConnectionCount { get; set; }
+
+        public ListenerInfo[] Listeners { get; set; }
+    }
+}
