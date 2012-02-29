0469b9fdf88d5a48ff403022b3b1f1bbb58c3d1c
kerryjiang <none@none.com>
2012-02-29 14:27:31 +0000

    mainline: checked in initial projects framework of "SuperSocket Server Manager"

---
+++ mainline/Management/Server/ManagementServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket;
+
+namespace SuperSocket.Management.Server
+{
+    public class ManagementServer : WebSocketServer<ManagementSession>
+    {
+
+    }
+}
---
+++ mainline/Management/Server/ManagementSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperWebSocket;
+
+namespace SuperSocket.Management.Server
+{
+    public class ManagementSession : WebSocketSession<ManagementSession>
+    {
+
+    }
+}
