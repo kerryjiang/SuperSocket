3445337761a294566381101269047cd6d1d40f33
kerryjiang <none@none.com>
2012-03-03 13:31:01 +0000

    mainline: tried to implement Server Manager

---
+++ mainline/Management/Client/WpfClient/Config/ServerConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Client.Config
+{
+    public class ServerConfig
+    {
+        public string Name { get; set; }
+
+        public string[] Instances { get; set; }
+    }
+}
