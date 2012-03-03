1c99f4b8c6c5584abd98ef88aaa509d22f40e8c0
kerryjiang <none@none.com>
2012-03-03 16:57:18 +0000

    mainline: tried to implement server manager

--- mainline/Management/Server/ManagementServer.cs
+++ mainline/Management/Server/ManagementServer.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
 using SuperWebSocket;
 
 namespace SuperSocket.Management.Server
 {
     public class ManagementServer : WebSocketServer<ManagementSession>
     {
+        private string m_ManagePassword;
 
+        public override bool Setup(IRootConfig rootConfig, IServerConfig config, ISocketServerFactory socketServerFactory, IRequestFilterFactory<WebSocketRequestInfo> protocol)
+        {
+            if (!base.Setup(rootConfig, config, socketServerFactory, protocol))
+                return false;
+
+            var password = config.Options.GetValue("managePassword");
+
+            if (string.IsNullOrEmpty(password))
+            {
+                Logger.Error("managePassword is required in configuration!");
+                return false;
+            }
+
+            m_ManagePassword = password;
+
+            return true;
+        }
     }
 }
