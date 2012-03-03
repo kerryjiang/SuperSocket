9fa6bc8ce6699472451c48b78413b6c993c53730
kerryjiang <none@none.com>
2012-03-03 13:31:01 +0000

    mainline: tried to implement Server Manager

---
+++ mainline/Management/Client/WpfClient/Model/ServerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Client.Model
+{
+    public class ServerModel
+    {
+        public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
