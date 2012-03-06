b6f89833dcb9cd1a21b0bfb0e97fcc517f0e0f21
kerryjiang <none@none.com>
2012-03-06 16:46:23 +0000

    mainline: tried to implement Server Manager

---
+++ mainline/Management/Client/WpfClient/Model/LoadingInstanceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Client.Model
+{
+    public class LoadingInstanceModel : InstanceModel
+    {
+
+    }
+}
