6468b1e4e0b354a3c3d14bea17bbc3f52a126c53
kerryjiang <none@none.com>
2012-03-21 17:44:12 +0000

    mainline: tried to implement server manager

---
+++ mainline/Management/Client/WpfClient/Converter/VisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SuperSocket.Management.Client.Converter
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool isChecked = (bool)value;
+            return isChecked ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
