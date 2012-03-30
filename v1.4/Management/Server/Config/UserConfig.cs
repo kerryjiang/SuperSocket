cc9e64fc65f7fc52f760e99e57876108168caf52
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Server/Config/UserConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.Common;
+using System.Configuration;
+
+namespace SuperSocket.Management.Server.Config
+{
+    public class UserConfig : ConfigurationElementBase
+    {
+        [ConfigurationProperty("password", IsRequired = true)]
+        public string Password
+        {
+            get
+            {
+                return (string)this["password"];
+            }
+        }
+    }
+}
---
+++ v1.4/Management/Server/Config/UserConfigCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace SuperSocket.Management.Server.Config
+{
+    [ConfigurationCollection(typeof(UserConfig))]
+    public class UserConfigCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new UserConfig();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((UserConfig)element).Name;
+        }
+
+        public new IEnumerator<UserConfig> GetEnumerator()
+        {
+            int count = base.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                yield return (UserConfig)base.BaseGet(i);
+            }
+        }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.BasicMap;
+            }
+        }
+
+        protected override string ElementName
+        {
+            get
+            {
+                return "user";
+            }
+        }
+    }
+}
