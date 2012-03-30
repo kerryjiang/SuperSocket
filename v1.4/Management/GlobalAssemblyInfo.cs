cff46996b2324d8db95d7b924e0b6db3dcb7c0ac
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SuperSocket")]
+[assembly: AssemblyProduct("SuperSocket Server Manager")]
+[assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
