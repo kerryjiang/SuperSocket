03d9cf2ed18b0899eed49c6d3036e64c5b2d4c2c
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Server/Command/LOGIN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using SuperSocket.Management.Shared;
+using SuperWebSocket;
+using SuperWebSocket.SubProtocol;
+
+namespace SuperSocket.Management.Server.Command
+{
+    public class LOGIN : JsonSubCommand<ManagementSession, LoginInfo>
+    {
+        protected override void ExecuteJsonCommand(ManagementSession session, LoginInfo commandInfo)
+        {
+            var user = session.AppServer.GetUserByName(commandInfo.UserName);
+
+            if (user == null || !EncryptPassword(user.Password).Equals(commandInfo.Password, StringComparison.OrdinalIgnoreCase))
+            {
+                SendJsonResponse(session, new LoginResult { Result = false });
+                return;
+            }
+
+            session.LoggedIn = true;
+            SendJsonResponse(session, new LoginResult { Result = true, ServerInfo = session.AppServer.CurrentServerInfo });
+        }
+
+        private string EncryptPassword(string password)
+        {
+            return Convert.ToBase64String(SHA1.Create().ComputeHash(Encoding.ASCII.GetBytes(password)));
+        }
+    }
+}
---
+++ v1.4/Management/Server/Command/STOP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperWebSocket.SubProtocol;
+using SuperSocket.SocketBase.Protocol;
+using SuperSocket.Management.Shared;
+using SuperSocket.Common;
+
+namespace SuperSocket.Management.Server.Command
+{
+    public class STOP : AsyncJsonSubCommand<ManagementSession, string>
+    {
+        protected override void ExecuteAsyncJsonCommand(ManagementSession session, string token, string commandInfo)
+        {
+            if (!session.LoggedIn)
+            {
+                session.Close();
+                return;
+            }
+
+            var server = session.AppServer.GetServerByName(commandInfo);
+
+            if (server == null)
+            {
+                SendJsonResponse(session, token, new StopResult { Result = false, Message = string.Format("The server instance \"{0}\" doesn't exist", commandInfo) });
+                return;
+            }
+
+            server.Stop();
+
+            SendJsonResponse(session, token, new StartResult { Result = true, ServerInfo = session.AppServer.GetUpdatedCurrentServerInfo() });
+        }
+    }
+}
