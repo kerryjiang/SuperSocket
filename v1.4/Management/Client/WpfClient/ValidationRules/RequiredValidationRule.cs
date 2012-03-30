becb53e799b364330f27d65457a73a39a3594a70
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Client/WpfClient/ValidationRules/RequiredValidationRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SuperSocket.Management.Client.ValidationRules
+{
+    public class RequiredValidationRule : ValidationRule
+    {
+        public RequiredValidationRule()
+        {
+
+        }
+
+        public string ErrorMessage { get; set; }
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+                return new ValidationResult(false, ErrorMessage);
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
