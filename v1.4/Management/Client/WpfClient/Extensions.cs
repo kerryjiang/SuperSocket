824b9a7e8333bcc615731dddf63c615a50d5a3b5
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Client/WpfClient/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace SuperSocket.Management.Client
+{
+    public static class Extensions
+    {
+        public static void XmlSerialize(this object target, string savePath)
+        {
+            var serializer = new XmlSerializer(target.GetType());
+
+            using (StreamWriter writer = new StreamWriter(savePath, false, Encoding.UTF8))
+            {
+                serializer.Serialize(writer, target);
+                writer.Close();
+            }
+        }
+
+        public static T XmlDeserialize<T>(this string xml)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+
+            using (var reader = new StringReader(xml))
+            {
+                var target = serializer.Deserialize(reader);
+
+                if(target == null)
+                    return default(T);
+
+                return (T)target;
+            }
+        }
+    }
+}
