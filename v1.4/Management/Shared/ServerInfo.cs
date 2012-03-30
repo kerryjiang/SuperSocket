0f9c8105572c6e4fe3c39c6fd4110c1b2581e4be
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Shared/ServerInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Shared
+{
+    public class ServerInfo
+    {
+        public double CpuUsage { get; set; }
+
+        public int AvailableWorkingThreads { get; set; }
+
+        public int AvailableCompletionPortThreads { get; set; }
+
+        public int MaxWorkingThreads { get; set; }
+
+        public int MaxCompletionPortThreads { get; set; }
+
+        public double PhysicalMemoryUsage { get; set; }
+
+        public int TotalThreadCount { get; set; }
+
+        public InstanceInfo[] Instances { get; set; }
+    }
+}
---
+++ v1.4/Management/Shared/StopResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Shared
+{
+    public class StopResult
+    {
+        public bool Result { get; set; }
+
+        public string Message { get; set; }
+
+        public ServerInfo ServerInfo { get; set; }
+    }
+}
