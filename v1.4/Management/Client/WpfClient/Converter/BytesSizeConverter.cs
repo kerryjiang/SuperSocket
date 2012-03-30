a1e0ca9676df42e1e7829c04fe759de2cb755667
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Client/WpfClient/Converter/BytesSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace SuperSocket.Management.Client.Converter
+{
+    public class BytesSizeConverter : IValueConverter
+    {
+        private BytesSizeFormatProvider m_FormatProvider = new BytesSizeFormatProvider();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return String.Format(m_FormatProvider, "{0:fs}", (double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
