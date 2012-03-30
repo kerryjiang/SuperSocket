f51adc6e948d9fa28ac4ffee851030d9552d01a4
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Client/WpfClient/Themes/Default/CircularProgressBar.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace SuperSocket.Management.Client.Themes.Default
+{
+    /// <summary>
+    /// Interaction logic for CircularProgressBar.xaml
+    /// </summary>
+    public partial class CircularProgressBar : UserControl
+    {
+        static CircularProgressBar()
+        {
+            //Use a default Animation Framerate of 30, which uses less CPU time
+            //than the standard 50 which you get out of the box
+            Timeline.DesiredFrameRateProperty.OverrideMetadata(
+                typeof(Timeline),
+                    new FrameworkPropertyMetadata { DefaultValue = 30 });
+        }
+
+        public CircularProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
