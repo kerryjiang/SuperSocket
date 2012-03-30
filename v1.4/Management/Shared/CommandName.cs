6de16d556707c3a8bcfc59cb6d7e678fca0c48d8
kerryjiang <none@none.com>
2012-03-30 13:24:14 +0000

    v1.4: implement server manager for v1.4

---
+++ v1.4/Management/Shared/CommandName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Shared
+{
+    public class CommandName
+    {
+        public const string UPDATE = "UPDATE";
+
+        public const string LOGIN = "LOGIN";
+
+        public const string START = "START";
+
+        public const string STOP = "STOP";
+    }
+}
---
+++ v1.4/Management/Shared/LoginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Management.Shared
+{
+    public class LoginInfo
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
