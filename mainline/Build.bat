825c9d430c8babc8a34bfb3b3599fa28c327f26d
kerryjiang <none@none.com>
2012-04-03 14:36:30 +0000

    mainline/v1.4: improved project files and build script

--- mainline/Build.bat
+++ mainline/Build.bat
@@ -8,16 +8,28 @@ if not exist %fdir% (
 
 set msbuild=%fdir%\v4.0.30319\msbuild.exe
 
-%msbuild% SuperSocket.2010.sln /p:Configuration=Debug /t:Rebuild
+%msbuild% SuperSocket.2010.sln /p:Configuration=Debug /t:Rebuild /p:OutputPath=..\bin\Net40\Debug
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
-%msbuild% SuperSocket.2010.sln /p:Configuration=Release /t:Rebuild
+%msbuild% SuperSocket.2010.sln /p:Configuration=Release /t:Rebuild /p:OutputPath=..\bin\Net40\Release
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
-%msbuild% SuperSocket.2010.NET35.sln /p:Configuration=Debug /t:Rebuild
+%msbuild% Management/Server/Server.csproj /p:Configuration=Debug /t:Rebuild /p:OutputPath=..\..\bin\Net40\Debug
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
-%msbuild% SuperSocket.2010.NET35.sln /p:Configuration=Release /t:Rebuild
+%msbuild% Management/Server/Server.csproj /p:Configuration=Release /t:Rebuild /p:OutputPath=..\..\bin\Net40\Release
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% SuperSocket.2010.NET35.sln /p:Configuration=Debug /t:Rebuild /p:OutputPath=..\bin\Net35\Debug
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% SuperSocket.2010.NET35.sln /p:Configuration=Release /t:Rebuild /p:OutputPath=..\bin\Net35\Release
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% Management/Server/Server.Net35.csproj /p:Configuration=Debug /t:Rebuild /p:OutputPath=..\..\bin\Net35\Debug
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% Management/Server/Server.Net35.csproj /p:Configuration=Release /t:Rebuild /p:OutputPath=..\..\bin\Net35\Release
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
 pause
