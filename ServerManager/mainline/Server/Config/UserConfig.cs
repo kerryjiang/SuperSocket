ca349ef3d788363f32a05bc46c1658bcdff84323
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/mainline/Server/Config/UserConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.Common;
+using System.Configuration;
+
+namespace SuperSocket.Management.Server.Config
+{
+    /// <summary>
+    /// User configuration
+    /// </summary>
+    public class UserConfig : ConfigurationElementBase
+    {
+        /// <summary>
+        /// Gets the password.
+        /// </summary>
+        [ConfigurationProperty("password", IsRequired = true)]
+        public string Password
+        {
+            get
+            {
+                return (string)this["password"];
+            }
+        }
+    }
+}
