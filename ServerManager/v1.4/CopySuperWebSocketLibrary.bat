5f6244bb11937c8f4c6d9fea897b9b0a30af783b
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/v1.4/CopySuperWebSocketLibrary.bat
@@ -0,0 +1,9 @@
+copy ..\..\..\SuperWebSocket\mainline\bin\Net40\Debug\SuperWebSocket.dll Reference\SuperWebSocket\NET40
+copy ..\..\..\SuperWebSocket\mainline\bin\Net40\Debug\SuperWebSocket.pdb Reference\SuperWebSocket\NET40
+copy ..\..\..\SuperWebSocket\mainline\bin\Net40\Debug\Newtonsoft.Json.dll Reference\SuperWebSocket\NET40
+
+copy ..\..\..\SuperWebSocket\mainline\bin\Net35\Debug\SuperWebSocket.dll Reference\SuperWebSocket\NET35
+copy ..\..\..\SuperWebSocket\mainline\bin\Net35\Debug\SuperWebSocket.pdb Reference\SuperWebSocket\NET35
+copy ..\..\..\SuperWebSocket\mainline\bin\Net35\Debug\Newtonsoft.Json.dll Reference\SuperWebSocket\NET35
+
+pause
