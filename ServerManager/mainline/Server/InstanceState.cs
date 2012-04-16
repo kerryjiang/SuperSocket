6821c23c34ad5f7582f139a42c09e14f3e579ab0
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/mainline/Server/InstanceState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.Management.Server
+{
+    /// <summary>
+    /// AppServer's instance
+    /// </summary>
+    public class InstanceState
+    {
+        /// <summary>
+        /// Gets or sets the instance.
+        /// </summary>
+        /// <value>
+        /// The instance.
+        /// </value>
+        public IAppServer Instance { get; set; }
+
+        /// <summary>
+        /// Gets or sets the performance stat.
+        /// </summary>
+        /// <value>
+        /// The performance.
+        /// </value>
+        public PerformanceData Performance { get; set; }
+    }
+}
