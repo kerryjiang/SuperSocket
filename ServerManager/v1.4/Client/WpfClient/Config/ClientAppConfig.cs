ade88e43fd319124b4e73f1fb96bbac4b6fcef1f
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/v1.4/Client/WpfClient/Config/ClientAppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperSocket.Management.Client.Config
+{
+    [XmlType("ClientApp")]
+    public class ClientAppConfig
+    {
+        [XmlArray]
+        public ServerConfig[] Servers { get; set; }
+
+        public string SecurityKey { get; set; }
+    }
+}
---
+++ ServerManager/v1.4/Client/WpfClient/Config/ServerConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SuperSocket.Management.Client.Config
+{
+    [XmlType("Server")]
+    public class ServerConfig
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("host")]
+        public string Host { get; set; }
+
+        [XmlAttribute("port")]
+        public int Port { get; set; }
+
+        [XmlAttribute("username")]
+        public string UserName { get; set; }
+
+        [XmlAttribute("password")]
+        public string Password { get; set; }
+    }
+}
