761c296acac2d3c2896262c11c54b4584105acdf
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/v1.4/Client/WpfClient/Selector/InstanceRowStyleSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using SuperSocket.Management.Client.ViewModel;
+
+namespace SuperSocket.Management.Client.Selector
+{
+    public class InstanceRowStyleSelector : StyleSelector
+    {
+        public Style NormalStyle { get; set; }
+
+        public Style LoadingStyle { get; set; }
+
+        public Style FaultStyle { get; set; }
+
+        public override Style SelectStyle(object item, System.Windows.DependencyObject container)
+        {
+            if (item is LoadingInstanceViewModel)
+                return LoadingStyle;
+            else if (item is FaultInstanceViewModel)
+                return FaultStyle;
+            else
+                return NormalStyle;
+        }
+    }
+}
