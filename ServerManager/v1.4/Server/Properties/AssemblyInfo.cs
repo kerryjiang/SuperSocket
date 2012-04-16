6dcb9359f651610847aadda6d9aa384b1d0b1fc5
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/v1.4/Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SuperSocket.Management.Server")]
+[assembly: AssemblyDescription("SuperSocket.Management.Server")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c2808866-1374-4663-b559-420266641a0a")]
