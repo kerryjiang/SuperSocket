ecc4a50227205b99bdcd083a3c655fbd1bf8f02f
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/mainline/Client/SlClient/Silverlight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SuperSocket.Management.Client
+{
+    public static class Silverlight
+    {
+        public static string GetSHA1Hash(string val)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(val);
+            System.Security.Cryptography.SHA1 sha = new System.Security.Cryptography.SHA1Managed();
+            byte[] res = sha.ComputeHash(data);
+            return Convert.ToBase64String(res);
+        }
+    }
+}
