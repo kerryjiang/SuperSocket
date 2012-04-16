d9af37c5e7a31d81e7a41321ab5c4f0fe7a82b27
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/mainline/Client/WpfClient/Converter/InstanceRowHeightConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using SuperSocket.Management.Client.ViewModel;
+
+namespace SuperSocket.Management.Client.Converter
+{
+    public class InstanceRowHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is LoadingInstanceViewModel)
+                return 0;
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServerManager/mainline/Client/WpfClient/Converter/RowTemplateConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using SuperSocket.Management.Client.ViewModel;
+
+namespace SuperSocket.Management.Client.Converter
+{
+    public class RowTemplateConverter : IValueConverter
+    {
+        public ControlTemplate NormalTemplate { get; set; }
+
+        public ControlTemplate LoadingTemplate { get; set; }
+
+        public ControlTemplate FaultTemplate { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is LoadingInstanceViewModel)
+                return LoadingTemplate;
+            else if (value is FaultInstanceViewModel)
+                return FaultTemplate;
+            else
+                return NormalTemplate;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
