446bc90a998904ea01aaa4c4ec123239584b7865
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/mainline/Client/SlClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SuperSocket.Management.Client")]
+[assembly: AssemblyDescription("SuperSocket.Management.Client for Silverlight")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fa39d865-8594-4073-999d-0de177759da5")]
