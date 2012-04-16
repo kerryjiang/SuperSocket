5ab0509b68383cb31c5b4fd3f8c86ba4899d0551
kerryjiang <none@none.com>
2012-04-16 12:21:12 +0000

    ServerManager: moved server manager to a new place

---
+++ ServerManager/v1.4/Client/WpfClient/Converter/ViewModelConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using SuperSocket.Management.Client.ViewModel;
+
+namespace SuperSocket.Management.Client.Converter
+{
+    public class ViewModelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var viewModelSource = value as Dictionary<string, IViewModelFactory>;
+
+            IViewModelFactory viewModelFactory;
+
+            if (!viewModelSource.TryGetValue(parameter.ToString(), out viewModelFactory))
+                throw new ArgumentException("parameter");
+
+            return viewModelFactory.CreateViewModel();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
