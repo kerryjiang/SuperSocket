baa904c492abeb8822b6c0f8938cad6e210c2148
kerryjiang <none@none.com>
2012-04-18 13:59:52 +0000

    mainline: removed the code of ClientEngine

--- mainline/ClientEngine/Properties/AssemblyInfo.Net40.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SuperSocket.ClientEngine for .NET 4.0/Mono 2.10+")]
-[assembly: AssemblyDescription("SuperSocket.ClientEngine")]
-[assembly: AssemblyConfiguration("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("fbbb2183-5bc3-4bbe-8f09-eb2d55d97d72")]
