63ccbd846657db22eb3ccf499b6e7b727c4fcf3b
kerryjiang <none@none.com>
2012-07-06 11:09:35 +0000

    v1.5: improved logging

---
+++ mainline/Common/Logging/LogFactoryBase.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace SuperSocket.Common.Logging
+{
+    /// <summary>
+    /// LogFactory Base class
+    /// </summary>
+    public abstract class LogFactoryBase : ILogFactory
+    {
+        /// <summary>
+        /// Gets the config file file path.
+        /// </summary>
+        protected string ConfigFile { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogFactoryBase"/> class.
+        /// </summary>
+        /// <param name="configFile">The config file.</param>
+        protected LogFactoryBase(string configFile)
+        {
+            if (Path.IsPathRooted(configFile))
+            {
+                ConfigFile = configFile;
+                return;
+            }
+
+            if (Path.DirectorySeparatorChar != '\\')
+            {
+                configFile = Path.GetFileNameWithoutExtension(configFile) + ".unix" + Path.GetExtension(configFile);
+            }
+
+            if (AppDomain.CurrentDomain.IsDefaultAppDomain())
+            {
+                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                filePath = Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config"), configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                ConfigFile = configFile;
+                return;
+            }
+            else //The running AppServer is in isolated appdomain
+            {
+                //1. search the appDomain's base directory
+                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                //go to the application's root
+                //the appdomain's root is /WorkingDir/DomainName, so get parent path twice to reach the application root
+                var rootDir = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.FullName;
+
+                //2. search the file with appdomain's name as prefix in the application's root
+                //the config file whose name have appDomain's name has higher priority
+                filePath = Path.Combine(rootDir, AppDomain.CurrentDomain.FriendlyName + "." + configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                //3. search in the application's root without appdomain's name as prefix
+                filePath = Path.Combine(rootDir, configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                
+                rootDir = Path.Combine(rootDir, "Config");
+                //Search the config file with appdomain's name as prefix in the Config dir
+                filePath = Path.Combine(rootDir, AppDomain.CurrentDomain.FriendlyName + "." + configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                filePath = Path.Combine(rootDir, configFile);
+
+                if (File.Exists(filePath))
+                {
+                    ConfigFile = filePath;
+                    return;
+                }
+
+                ConfigFile = configFile;
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Gets the log by name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public abstract ILog GetLog(string name);
+    }
+}
