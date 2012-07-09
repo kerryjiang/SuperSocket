9188a4c8e485a98895cdc388e8b21190027e5bb3
kerryjiang <none@none.com>
2012-07-09 17:38:11 +0000

    v1.5: tried to implement AppDomain level isolation

---
+++ mainline/Common/ErrorEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.Common
+{
+    /// <summary>
+    /// EventArgs for error and exception
+    /// </summary>
+    public class ErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the exception.
+        /// </summary>
+        public Exception Exception { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public ErrorEventArgs(string message)
+        {
+            Exception = new Exception(message);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorEventArgs"/> class.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        public ErrorEventArgs(Exception exception)
+        {
+            Exception = exception;
+        }
+    }
+}
---
+++ mainline/SocketBase/IsolationMode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.SocketBase
+{
+    /// <summary>
+    /// AppServer instance running isolation mode
+    /// </summary>
+    public enum IsolationMode
+    {
+        /// <summary>
+        /// No isolation
+        /// </summary>
+        None,
+        /// <summary>
+        /// Isolation by AppDomain
+        /// </summary>
+        AppDomain
+    }
+}
