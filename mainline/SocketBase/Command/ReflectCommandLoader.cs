630e55e57ddf4f8dc33e2b12d7f6fb044055cb19
kerryjiang <none@none.com>
2012-07-11 16:49:05 +0000

    v1.5: removed useless comment

--- mainline/SocketBase/Command/ReflectCommandLoader.cs
+++ mainline/SocketBase/Command/ReflectCommandLoader.cs
@@ -13,55 +13,6 @@ namespace SuperSocket.SocketBase.Command
     /// </summary>
     public class ReflectCommandLoader : CommandLoaderBase
     {
-        //#region ICommandLoader Members
-
-        ///// <summary>
-        ///// Loads the commands for specific server.
-        ///// </summary>
-        ///// <typeparam name="TAppSession">The type of the app session.</typeparam>
-        ///// <typeparam name="TRequestInfo">The type of the request info.</typeparam>
-        ///// <param name="appServer">The app server.</param>
-        ///// <param name="commandRegister">The command register.</param>
-        ///// <param name="commandUpdater">The command updater.</param>
-        ///// <returns></returns>
-        //public bool LoadCommands<TAppSession, TRequestInfo>(IAppServer appServer, Func<ICommand<TAppSession, TRequestInfo>, bool> commandRegister, Action<IEnumerable<CommandUpdateInfo<ICommand<TAppSession, TRequestInfo>>>> commandUpdater)
-        //    where TAppSession : IAppSession, IAppSession<TAppSession, TRequestInfo>, new()
-        //    where TRequestInfo : IRequestInfo
-        //{
-        //    var commandAssemblies = new List<Assembly> { appServer.GetType().Assembly };
-
-        //    string commandAssembly = appServer.Config.Options.GetValue("commandAssembly");
-
-        //    if (!string.IsNullOrEmpty(commandAssembly))
-        //    {
-        //        try
-        //        {
-        //            var definedAssemblies = AssemblyUtil.GetAssembliesFromString(commandAssembly);
-
-        //            if (definedAssemblies.Any())
-        //                commandAssemblies.AddRange(definedAssemblies);
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            OnError(new Exception("Failed to load defined command assemblies!", e));
-        //            return false;
-        //        }
-        //    }
-
-        //    foreach (var assembly in commandAssemblies)
-        //    {
-        //        foreach (var c in assembly.GetImplementedObjectsByInterface<ICommand<TAppSession, TRequestInfo>>())
-        //        {
-        //            if (!commandRegister(c))
-        //                return false;
-        //        }
-        //    }
-
-        //    return true;
-        //}
-
-        //#endregion
-
         private Type m_CommandType;
 
         private IAppServer m_AppServer;
