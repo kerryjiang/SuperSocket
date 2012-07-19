9b643c5b959ea6a97387c9ed563f92ec696ff592
kerryjiang <none@none.com>
2012-07-19 08:32:43 +0000

    v1.5: checked in missed files

---
+++ mainline/SocketBase/ILoggerProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Logging;
+
+namespace SuperSocket.SocketBase
+{
+    /// <summary>
+    /// The interface for who provides logger
+    /// </summary>
+    public interface ILoggerProvider
+    {
+        /// <summary>
+        /// Gets the logger assosiated with this object.
+        /// </summary>
+        ILog Logger { get; }
+    }
+}
