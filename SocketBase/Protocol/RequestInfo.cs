785a946d51391bc903f3ed9300f1359b14c7d394
Kerry Jiang <kerry-jiang@hotmail.com>
2012-08-11 22:34:40 +0800

    added RequestInfo basic class with header and body

--- SocketBase/Protocol/RequestInfo.cs
+++ SocketBase/Protocol/RequestInfo.cs
@@ -11,12 +11,27 @@ namespace SuperSocket.SocketBase.Protocol
     /// <typeparam name="TRequestBody">The type of the request body.</typeparam>
     public class RequestInfo<TRequestBody> : IRequestInfo<TRequestBody>
     {
+        protected RequestInfo()
+        {
+            
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RequestInfo&lt;TRequestBody&gt;"/> class.
         /// </summary>
         /// <param name="key">The key.</param>
         /// <param name="body">The body.</param>
-        protected RequestInfo(string key, TRequestBody body)
+        public RequestInfo(string key, TRequestBody body)
+        {
+            Initialize(key, body);
+        }
+
+        /// <summary>
+        /// Initializes the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="body">The body.</param>
+        protected void Initialize(string key, TRequestBody body)
         {
             Key = key;
             Body = body;
@@ -31,5 +46,48 @@ namespace SuperSocket.SocketBase.Protocol
         /// Gets the body.
         /// </summary>
         public TRequestBody Body { get; private set; }
+    }
+
+    /// <summary>
+    /// RequestInfo with header
+    /// </summary>
+    /// <typeparam name="TRequestHeader">The type of the request header.</typeparam>
+    /// <typeparam name="TRequestBody">The type of the request body.</typeparam>
+    public class RequestInfo<TRequestHeader, TRequestBody> : RequestInfo<TRequestBody>, IRequestInfo<TRequestHeader, TRequestBody>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestInfo&lt;TRequestHeader, TRequestBody&gt;"/> class.
+        /// </summary>
+        public RequestInfo()
+        {
+            
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestInfo&lt;TRequestHeader, TRequestBody&gt;"/> class.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="header">The header.</param>
+        /// <param name="body">The body.</param>
+        public RequestInfo(string key, TRequestHeader header, TRequestBody body)
+            : base(key, body)
+        {
+            Header = header;
+        }
+
+        /// <summary>
+        /// Initializes the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="header">The header.</param>
+        /// <param name="body">The body.</param>
+        public void Initialize(string key, TRequestHeader header, TRequestBody body)
+        {
+            base.Initialize(key, body);
+            Header = header;
+        }
+        /// <summary>
+        /// Gets the header.
+        /// </summary>
+        public TRequestHeader Header { get; private set; }
     }
 }
