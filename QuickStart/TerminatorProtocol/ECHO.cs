365c27c73b5278bd49b29c8fca6bf73639dc607c
Kerry Jiang <kerry-jiang@hotmail.com>
2012-08-14 17:21:10 +0800

    added log4net config files for quickstart projects

---
+++ QuickStart/TerminatorProtocol/ECHO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.TerminatorProtocol
+{
+    public class ECHO : StringCommandBase
+    {
+        public override void ExecuteCommand(AppSession session, StringRequestInfo requestInfo)
+        {
+            session.Send(requestInfo.Body);
+        }
+    }
+}
