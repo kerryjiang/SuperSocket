9a80b1fbc6c2960c2fcbc9ab813bab489e0e3c4f
Kerry Jiang <kerry-jiang@hotmail.com>
2012-10-10 00:30:40 +0800

    improved the code about server state

--- SocketBase/GlobalPerformanceData.cs
+++ SocketBase/GlobalPerformanceData.cs
@@ -12,51 +12,12 @@ namespace SuperSocket.SocketBase
     public class GlobalPerformanceData
     {
         /// <summary>
-        /// Gets or sets the available working threads.
-        /// </summary>
-        /// <value>
-        /// The available working threads.
-        /// </value>
-        public int AvailableWorkingThreads { get; set; }
-
-        /// <summary>
-        /// Gets or sets the available completion port threads.
-        /// </summary>
-        /// <value>
-        /// The available completion port threads.
-        /// </value>
-        public int AvailableCompletionPortThreads { get; set; }
-
-        /// <summary>
-        /// Gets or sets the max working threads.
-        /// </summary>
-        /// <value>
-        /// The max working threads.
-        /// </value>
-        public int MaxWorkingThreads { get; set; }
-
-        /// <summary>
-        /// Gets or sets the max completion port threads.
-        /// </summary>
-        /// <value>
-        /// The max completion port threads.
-        /// </value>
-        public int MaxCompletionPortThreads { get; set; }
-
-        /// <summary>
-        /// Gets or sets the total thread count.
-        /// </summary>
-        /// <value>
-        /// The total thread count.
-        /// </value>
-        public int TotalThreadCount { get; set; }
-
-        /// <summary>
         /// Gets or sets the cpu usage.
         /// </summary>
         /// <value>
         /// The cpu usage.
         /// </value>
+        [Display(Name = "CPU Usage", Format = "{0:0.00}%", Order = 0)]
         public double CpuUsage { get; set; }
 
         /// <summary>
@@ -65,6 +26,52 @@ namespace SuperSocket.SocketBase
         /// <value>
         /// The working set.
         /// </value>
+        [Display(Name = "Physical Memory Usage", Format = "{0:N}", Order = 1)]
         public long WorkingSet { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the total thread count.
+        /// </summary>
+        /// <value>
+        /// The total thread count.
+        /// </value>
+        [Display(Name = "Total Thread Count", Order = 2)]
+        public int TotalThreadCount { get; set; }
+        /// <summary>
+        /// Gets or sets the available working threads.
+        /// </summary>
+        /// <value>
+        /// The available working threads.
+        /// </value>
+        [Display(Name = "Available Working Threads", Order = 3)]
+        public int AvailableWorkingThreads { get; set; }
+
+        /// <summary>
+        /// Gets or sets the available completion port threads.
+        /// </summary>
+        /// <value>
+        /// The available completion port threads.
+        /// </value>
+        [Display(Name = "Available Completion Port Threads", Order = 4)]
+        public int AvailableCompletionPortThreads { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max working threads.
+        /// </summary>
+        /// <value>
+        /// The max working threads.
+        /// </value>
+        [Display(Name = "Maximum Working Threads", , Order = 5)]
+        public int MaxWorkingThreads { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max completion port threads.
+        /// </summary>
+        /// <value>
+        /// The max completion port threads.
+        /// </value>
+        [Display(Name = "Maximum Completion Port Threads", Order = 6)]
+        public int MaxCompletionPortThreads { get; set; }
     }
 }
