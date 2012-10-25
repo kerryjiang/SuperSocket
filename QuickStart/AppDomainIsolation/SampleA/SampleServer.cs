64da756c599d578f4103b46764a7911c24d2d4a3
Kerry Jiang <kerry-jiang@hotmail.com>
2012-10-25 23:52:56 +0800

    added more sample projects in QuickStart for AppDomain level isolation

---
+++ QuickStart/AppDomainIsolation/SampleA/SampleServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+
+namespace SuperSocket.QuickStart.AppDomainIsolation.SampleA
+{
+    public class SampleServer : AppServer
+    {
+
+    }
+}
---
+++ QuickStart/AppDomainIsolation/SampleB/Command/CLOSE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace SuperSocket.QuickStart.AppDomainIsolation.SampleB.Command
+{
+    public class CLOSE : StringCommandBase
+    {
+        public override void ExecuteCommand(AppSession session, StringRequestInfo requestInfo)
+        {
+            session.Close();
+        }
+    }
+}
