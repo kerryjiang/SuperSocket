08ea506f9068757b9a88b73db35a1b6c77f48233
Kerry Jiang <kerry-jiang@hotmail.com>
2013-01-08 18:06:52 +0800

    updated the assembly version to 1.5.1

--- Solution Items/GlobalAssemblyInfo.cs
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.0.7")]
-[assembly: AssemblyFileVersion("1.5.0.7")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket")]
 [assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2010-2013")]
