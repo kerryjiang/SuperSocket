dee9157ad7cc2674ac3d96ab688d73fe1aaf23c4
Kerry Jiang <kerry-jiang@hotmail.com>
2013-05-14 09:48:16 +0800

    added a missing class in Test project

---
+++ Test/Command/PROC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+using System.Diagnostics;
+
+namespace SuperSocket.Test.Command
+{
+    public class PROC : StringCommandBase<TestSession>
+    {
+        public override void ExecuteCommand(TestSession session, StringRequestInfo requestInfo)
+        {
+            session.Send(Process.GetCurrentProcess().ProcessName);
+        }
+    }
+}
