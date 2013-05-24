809f457284b008494ae9571e51b10655231b6782
Kerry Jiang <kerry-jiang@hotmail.com>
2013-05-24 12:04:30 +0800

    fixed a bug which can cause the closed event will be fired multiple times

--- SocketEngine/SocketSession.cs
+++ SocketEngine/SocketSession.cs
@@ -20,7 +20,7 @@ namespace SuperSocket.SocketEngine
     {
         public const int Normal = 0;//0000 0000
         public const int InClosing = 16;//0001 0000  >= 16
-        public static int Closed = 16777216;//256 * 256 * 256; 0x01 0x00 0x00 0x00
+        public const int Closed = 16777216;//256 * 256 * 256; 0x01 0x00 0x00 0x00
         public const int InSending = 1;//0000 0001  > 1
         public const int InReceiving = 2;//0000 0010 > 2
         public const int InSendingReceivingMask = -4;// ~(InSending | InReceiving); 0xf0 0xff 0xff 0xff
@@ -162,7 +162,9 @@ namespace SuperSocket.SocketEngine
         /// </summary>
         protected virtual void OnClosed(CloseReason reason)
         {
-            AddStateFlag(SocketState.Closed);
+            //Already closed
+            if (!TryAddStateFlag(SocketState.Closed))
+                return;
 
             //Before changing m_SendingQueue, must check m_IsClosed
             while (true)
