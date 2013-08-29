ce9f1826d50c8ef2a42ce9553700f5796c6f3573
Kerry Jiang <kerry-jiang@hotmail.com>
2013-08-29 22:09:39 +0800

    added the code changing the SuperSocket control script to be executable in Linux

--- SocketService/Program.cs
+++ SocketService/Program.cs
@@ -24,7 +24,11 @@ namespace SuperSocket.SocketService
         /// The main entry point for the application.
         /// </summary>
         static void Main(string[] args)
-        {            
+        {
+            //If this application run in Mono/Linux, change the control script to be executable
+            if(Platform.IsMono && Path.DirectorySeparatorChar == '/')
+                ChangeScriptExecutable();
+
             if ((!Platform.IsMono && !Environment.UserInteractive)//Windows Service
                 || (Platform.IsMono && !AppDomain.CurrentDomain.FriendlyName.Equals(Path.GetFileName(Assembly.GetEntryAssembly().CodeBase))))//MonoService
             {
@@ -60,6 +64,23 @@ namespace SuperSocket.SocketService
                     exeArg = exeArg.TrimStart('-');
 
                 Run(exeArg, args);
+            }
+        }
+
+        static void ChangeScriptExecutable()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "supersocket.sh");
+
+            try
+            {
+                if (!File.Exists(filePath))
+                    return;
+
+                File.SetAttributes(filePath, (FileAttributes)((uint)File.GetAttributes(filePath) | 0x80000000));
+            }
+            catch
+            {
+
             }
         }
 
@@ -355,7 +376,7 @@ namespace SuperSocket.SocketService
             }
             catch (Exception e)
             {
-                Console.WriteLine("Failed. " + e.Message);
+                Console.WriteLine("Failed. " + e.Message + Environment.NewLine + e.StackTrace);
             }
 
             ReadConsoleCommand(bootstrap);
