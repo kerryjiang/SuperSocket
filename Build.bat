53cbb8347ce563c1da17e79e723fcdd6296c8508
Kerry Jiang <kerry-jiang@hotmail.com>
2013-10-24 23:35:53 +0800

    improved the build scripts

--- Build.bat
+++ Build.bat
@@ -20,4 +20,13 @@ FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 %msbuild% SuperSocket.2010.NET35.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net35\Release
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.5" 2>nul
+if errorlevel 0 (
+    %msbuild% SuperSocket.2012.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Debug
+	FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+	%msbuild% SuperSocket.2012.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Release
+	FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+)
+
 pause
