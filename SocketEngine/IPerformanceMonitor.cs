df2390294c148f1c4d2391854a187e9bb9b3a68f
Cansın <cansin.senalioglu@gmail.com>
2014-03-13 16:11:36 +0200

    Added IPerformanceMonitor for public.
    Conflicts:
    
    	SocketEngine/SuperSocket.SocketEngine.Net40.csproj

---
+++ SocketEngine/IPerformanceMonitor.cs
@@ -0,0 +1,27 @@
+﻿using SuperSocket.SocketBase;
+using System;
+namespace SuperSocket.SocketEngine
+{
+    /// <summary>
+    /// Interface of IPerformanceMonitor
+    /// </summary>
+    public interface IPerformanceMonitor : IDisposable
+    {
+        /// <summary>
+        /// Start PerformanceMonitor.
+        /// </summary>
+        void Start();
+        /// <summary>
+        /// Stop PerformanceMonitor.
+        /// </summary>
+        void Stop();
+        /// <summary>
+        /// Invoke when status update.
+        /// </summary>
+        event Action<NodeStatus> onUpdate;
+        /// <summary>
+        /// Get or Set the current refresh timer in seconds.
+        /// </summary>
+        int TimerInterval { get; set; }
+    }
+}
