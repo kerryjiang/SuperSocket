63b4ff18b5ecff8cfa0207bdb6e91567285c52a8
Kerry Jiang <kerry-jiang@hotmail.com>
2014-05-03 13:04:58 +0800

    supported AppContext in the ReceiveFilters

---
+++ SocketBase/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SuperSocket.SocketBase
+{
+    public class AppContext
+    {
+        private const string m_SesionDataSlotName = "Session";
+
+        public static IAppSession CurrentSession
+        {
+            get
+            {
+                return Thread.GetData(Thread.GetNamedDataSlot(m_SesionDataSlotName)) as IAppSession;
+            }
+        }
+
+        internal static LocalDataStoreSlot SetCurrentSession(IAppSession session)
+        {
+            var slot = Thread.GetNamedDataSlot(m_SesionDataSlotName);
+            Thread.SetData(slot, session);
+            return slot;
+        }
+    }
+}
