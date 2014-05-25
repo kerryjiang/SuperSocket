5e714cd80378c343933822b96dff9b680eb26468
Kerry Jiang <kerry-jiang@hotmail.com>
2014-05-25 22:55:17 +0800

    added back the missing classes in the ProtoBase

---
+++ ProtoBase/IBufferRecycler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.ProtoBase
+{
+    public interface IBufferRecycler
+    {
+        void Return(IList<KeyValuePair<ArraySegment<byte>, IBufferState>> buffers, int offset, int length);
+    }
+
+    class NullBufferRecycler : IBufferRecycler
+    {
+        public void Return(IList<KeyValuePair<ArraySegment<byte>, IBufferState>> buffers, int offset, int length)
+        {
+            //Do nothing
+        }
+    }
+}
---
+++ ProtoBase/IBufferState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SuperSocket.ProtoBase
+{
+    public interface IBufferState
+    {
+        int DecreaseReference();
+
+        void IncreaseReference();
+    }
+
+    public abstract class BufferBaseState : IBufferState
+    {
+        private int m_Reference;
+        
+        public int DecreaseReference()
+        {
+            return Interlocked.Decrement(ref m_Reference) - 1;
+        }
+
+        public void IncreaseReference()
+        {
+            Interlocked.Increment(ref m_Reference);
+        }
+    }
+}
