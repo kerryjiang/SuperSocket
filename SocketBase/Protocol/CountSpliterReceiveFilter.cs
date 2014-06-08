761ccff5d35144a555f17b0f635ff96ea1cff969
Kerry Jiang <kerry-jiang@hotmail.com>
2014-06-08 23:42:16 +0800

    added more classes for CountSpliterReceiveFilter

---
+++ SocketBase/Protocol/CountSpliterReceiveFilter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperSocket.ProtoBase;
+
+namespace SuperSocket.SocketBase.Protocol
+{
+    /// <summary>
+    /// This Receive filter is designed for this kind protocol:
+    /// each request has fixed count part which splited by a char(byte)
+    /// for instance, request is defined like this "#12122#23343#4545456565#343435446#",
+    /// because this request is splited into many parts by 5 '#', we can create a Receive filter by CountSpliterRequestFilter((byte)'#', 5)
+    /// </summary>
+    public class CountSpliterReceiveFilter : CountSpliterReceiveFilter<StringRequestInfo>
+    {
+        private readonly Encoding m_Encoding;
+
+        private readonly int m_KeyIndex;
+
+        private readonly char m_Spliter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountSpliterReceiveFilter"/> class.
+        /// </summary>
+        /// <param name="spliter">The spliter.</param>
+        /// <param name="spliterCount">The spliter count.</param>
+        public CountSpliterReceiveFilter(byte[] spliter, int spliterCount)
+            : this(spliter, spliterCount, Encoding.ASCII)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountSpliterReceiveFilter"/> class.
+        /// </summary>
+        /// <param name="spliter">The spliter.</param>
+        /// <param name="spliterCount">The spliter count.</param>
+        /// <param name="encoding">The encoding.</param>
+        public CountSpliterReceiveFilter(byte[] spliter, int spliterCount, Encoding encoding)
+            : this(spliter, spliterCount, encoding, 0)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountSpliterReceiveFilter"/> class.
+        /// </summary>
+        /// <param name="spliter">The spliter.</param>
+        /// <param name="spliterCount">The spliter count.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <param name="keyIndex">Index of the key.</param>
+        public CountSpliterReceiveFilter(byte[] spliter, int spliterCount, Encoding encoding, int keyIndex)
+            : base(spliter, spliterCount)
+        {
+            m_Encoding = encoding;
+            m_KeyIndex = keyIndex;
+            m_Spliter = (char)spliter[0];
+        }
+
+        /// <summary>
+        /// Resolves the package.
+        /// </summary>
+        /// <param name="packageData">The package data.</param>
+        /// <returns></returns>
+        public override StringRequestInfo ResolvePackage(IList<ArraySegment<byte>> packageData)
+        {
+            //ignore the first and the last spliter
+            var total = packageData.Sum(d => d.Count);
+            var body = m_Encoding.GetString(packageData, 1, total - 2);
+            var array = body.Split(m_Spliter);
+            return new StringRequestInfo(array[m_KeyIndex], body, array);
+        }
+    }
+}
---
+++ SocketBase/Protocol/CountSpliterReceiveFilterFactory.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using SuperSocket.ProtoBase;
+
+namespace SuperSocket.SocketBase.Protocol
+{
+    /// <summary>
+    /// ReceiveFilterFactory for CountSpliterReceiveFilter
+    /// </summary>
+    /// <typeparam name="TReceiveFilter">The type of the Receive filter.</typeparam>
+    /// <typeparam name="TPackageInfo">The type of the request info.</typeparam>
+    public class CountSpliterReceiveFilterFactory<TReceiveFilter, TPackageInfo> : IReceiveFilterFactory<TPackageInfo>
+        where TReceiveFilter : CountSpliterReceiveFilter<TPackageInfo>, new()
+        where TPackageInfo : IRequestInfo
+    {
+        /// <summary>
+        /// Creates the filter.
+        /// </summary>
+        /// <param name="appServer">The app server.</param>
+        /// <param name="appSession">The app session.</param>
+        /// <param name="remoteEndPoint">The remote end point.</param>
+        /// <returns></returns>
+        public IReceiveFilter<TPackageInfo> CreateFilter(IAppServer appServer, IAppSession appSession, IPEndPoint remoteEndPoint)
+        {
+            var config = appServer.Config;
+
+            if (config.MaxRequestLength > config.ReceiveBufferSize)
+                throw new Exception("ReceiveBufferSize cannot smaller than MaxRequestLength in this protocol.");
+
+            return new TReceiveFilter();
+        }
+    }
+
+    /// <summary>
+    /// ReceiveFilterFactory for CountSpliterReceiveFilter
+    /// </summary>
+    /// <typeparam name="TReceiveFilter">The type of the Receive filter.</typeparam>
+    public class CountSpliterReceiveFilterFactory<TReceiveFilter> : CountSpliterReceiveFilterFactory<TReceiveFilter, StringRequestInfo>
+        where TReceiveFilter : CountSpliterReceiveFilter<StringRequestInfo>, new()
+    {
+
+    }
+
+    /// <summary>
+    /// receiveFilterFactory for CountSpliterRequestFilter
+    /// </summary>
+    public class CountSpliterReceiveFilterFactory : IReceiveFilterFactory<StringRequestInfo>
+    {
+        private readonly byte[] m_Spliter;
+
+        private readonly int m_SpliterCount;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountSpliterReceiveFilterFactory"/> class.
+        /// </summary>
+        /// <param name="spliter">The spliter.</param>
+        /// <param name="count">The count.</param>
+        public CountSpliterReceiveFilterFactory(byte spliter, int count)
+        {
+            m_Spliter = new byte[] { spliter };
+            m_SpliterCount = count;
+        }
+
+        /// <summary>
+        /// Creates the filter.
+        /// </summary>
+        /// <param name="appServer">The app server.</param>
+        /// <param name="appSession">The app session.</param>
+        /// <param name="remoteEndPoint">The remote end point.</param>
+        /// <returns></returns>
+        public IReceiveFilter<StringRequestInfo> CreateFilter(IAppServer appServer, IAppSession appSession, IPEndPoint remoteEndPoint)
+        {
+            return new CountSpliterReceiveFilter(m_Spliter, m_SpliterCount);
+        }
+    }
+}
