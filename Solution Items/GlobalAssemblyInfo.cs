4002b40818503da3c8fb256e48ddbbfc55eafeb8
Kerry Jiang <kerry-jiang@hotmail.com>
2014-06-26 23:42:08 +0800

    upgraded version to 1.6.3

--- Solution Items/GlobalAssemblyInfo.cs
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
+[assembly: AssemblyVersion("1.6.3.0")]
+[assembly: AssemblyFileVersion("1.6.3.0")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket")]
 [assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2010-2014")]
