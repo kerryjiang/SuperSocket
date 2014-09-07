f49c349c9e655a97b28ac9f962046dee93d68c94
Kerry Jiang <kerry-jiang@hotmail.com>
2014-09-06 17:04:31 -0700

    removed the signature settings from project files, all signature properties will be pass in by command parameters, signing certificate also has been removed from the repository

---
+++ BuildWithSignature.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set fdir=%WINDIR%\Microsoft.NET\Framework64
+
+if not exist %fdir% (
+	set fdir=%WINDIR%\Microsoft.NET\Framework
+)
+
+set msbuild=%fdir%\v4.0.30319\msbuild.exe
+
+%msbuild% SuperSocket.2010.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\Net40\Debug /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%CD%\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% SuperSocket.2010.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net40\Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%CD%\supersocket.snk
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.5" 2>nul
+if errorlevel 0 (
+    %msbuild% SuperSocket.2012.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Debug /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%CD%\supersocket.snk
+	FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+	%msbuild% SuperSocket.2012.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=%CD%\supersocket.snk
+	FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+)
+
+pause
--- Push.bat
+++ Push.bat
@@ -1,3 +1,3 @@
 git push origin master:master
-git push github master:master
+git push codeplex master:master
 pause
