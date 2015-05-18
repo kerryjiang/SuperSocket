90da5fa73a1fc1adef99f0f48bc495f773ce4969
Kerry Jiang <kerry-jiang@hotmail.com>
2015-05-19 00:24:32 +0800

    fixed the missing solution files

--- Build.bat
+++ Build.bat
@@ -22,10 +22,10 @@ FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.5" 2>nul
 if errorlevel 0 (
-    %msbuild% SuperSocket.2012.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Debug
+    %msbuild% SuperSocket.2013.sln /p:Configuration=Debug /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Debug
 	FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
-	%msbuild% SuperSocket.2012.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Release
+	%msbuild% SuperSocket.2013.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net45\Release
 	FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 )
 
