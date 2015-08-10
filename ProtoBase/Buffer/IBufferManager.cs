0c309886a81de215375f32822296b2d6ece25cfd
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-10 23:02:05 +0800

    re-organized ProtoBase project

---
+++ ProtoBase/Buffer/IBufferManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace SuperSocket.ProtoBase
+{
+    /// <summary>
+    /// The buffer manager interface
+    /// </summary>
+    public interface IBufferManager
+    {
+        /// <summary>
+        /// Gets the buffer.
+        /// </summary>
+        /// <param name="size">The size of the resired buffer.</param>
+        /// <returns></returns>
+        byte[] GetBuffer(int size);
+
+        /// <summary>
+        /// Returns the buffer.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        void ReturnBuffer(byte[] buffer);
+
+        /// <summary>
+        /// Shrinks this instance.
+        /// </summary>
+        void Shrink();
+    }
+}
---
+++ ProtoBase/Buffer/NullBufferManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.ProtoBase
+{
+    /// <summary>
+    /// Runtime memory assignment buffer manager (no buffer)
+    /// </summary>
+    public class NullBufferManager : IBufferManager
+    {
+        /// <summary>
+        /// Gets the buffer.
+        /// </summary>
+        /// <param name="size">The size of the resired buffer.</param>
+        /// <returns></returns>
+        public byte[] GetBuffer(int size)
+        {
+            return new byte[size];
+        }
+
+        /// <summary>
+        /// Returns the buffer.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        public void ReturnBuffer(byte[] buffer)
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Shrinks this instance.
+        /// </summary>
+        public void Shrink()
+        {
+            // do nothing
+        }
+    }
+}
