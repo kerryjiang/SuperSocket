69c03f5930dbb0042c76364ded63130e9f13fbef
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-17 17:43:20 +0800

    added the interface communication channel into ProtoBase project

---
+++ ProtoBase/ICommunicationChannel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.ProtoBase
+{
+    /// <summary>
+    /// The interface for communication channel
+    /// </summary>
+    public interface ICommunicationChannel
+    {
+        /// <summary>
+        /// Send the binary segment to the other endpoint through this communication channel
+        /// </summary>
+        /// <param name="segment">the data segment to be sent</param>
+        void Send(ArraySegment<byte> segment);
+
+        /// <summary>
+        /// Close the communication channel
+        /// </summary>
+        void Close();
+    }
+}
