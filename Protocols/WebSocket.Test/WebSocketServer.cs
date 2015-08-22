f3fc2eb58d610a959c366c46b78759bb6e10031b
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-22 22:11:09 +0800

    improved the test project of WebSocket

---
+++ Protocols/WebSocket.Test/WebSocketServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.SocketBase;
+
+namespace WebSocket.Test
+{
+    public class WebSocketServer : AppServer
+    {
+
+    }
+}
