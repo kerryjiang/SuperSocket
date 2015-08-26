a786ad813886dc83e626d213f7185330d639cdf4
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-27 00:23:05 +0800

    added the protocol encoder interfaces

---
+++ ProtoBase/Encoder/IProtoDataEncoder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.ProtoBase
+{
+    /// <summary>
+    /// The interface for protocol encoder of binary data
+    /// </summary>
+    public interface IProtoDataEncoder
+    {
+        /// <summary>
+        /// Encode the binary data
+        /// </summary>
+        /// <param name="data">the binary data to be encoded</param>
+        /// <returns>the output binary data</returns>
+        IList<ArraySegment<byte>> EncodeData(ArraySegment<byte> data);
+
+        /// <summary>
+        /// Encode the binary segments
+        /// </summary>
+        /// <param name="data">the binary segments to be encoded</param>
+        /// <returns>the output binary data</returns>
+        IList<ArraySegment<byte>> EncodeData(IList<ArraySegment<byte>> data);
+    }
+}
---
+++ ProtoBase/Encoder/IProtoTextEncoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocket.ProtoBase
+{
+    /// <summary>
+    /// The interface for protocol encoder of text messages
+    /// </summary>
+    public interface IProtoTextEncoder
+    {
+        /// <summary>
+        /// Encode text message
+        /// </summary>
+        /// <param name="message">the message to be encoded</param>
+        /// <returns>the output binary data</returns>
+        IList<ArraySegment<byte>> EncodeText(string message);
+    }
+}
