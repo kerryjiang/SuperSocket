2e7598b0325dbe224a1eba6ca5b5af3672245104
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-27 23:55:41 +0800

    added the missing DefaultProtoSender class

---
+++ SocketBase/Protocol/DefaultProtoSender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSocket.SocketBase.Protocol
+{
+    class DefaultProtoSender : IProtoSender
+    {
+        public void Send(ISocketSession session, IList<ArraySegment<byte>> segments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(ISocketSession session, ArraySegment<byte> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TrySend(ISocketSession session, IList<ArraySegment<byte>> segments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TrySend(ISocketSession session, ArraySegment<byte> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
