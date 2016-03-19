f460899c3582f7e8a7e90f4e482900d86c17c5cf
Kerry Jiang <kerry-jiang@hotmail.com>
2016-03-20 00:39:50 +0800

    tried to implement a simpler sending queue: SendingQueueV2

---
+++ SocketBase/Utils/ISendingQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.ProtoBase;
+
+namespace SuperSocket.SocketBase
+{
+    /// <summary>
+    /// The inertface for sending queue
+    /// </summary>
+    public interface ISendingQueue : IList<ArraySegment<byte>>, IOutputBuffer
+    {
+
+    }
+}
--- SocketBase/Utils/SendingQueueV2.cs
+++ SocketBase/Utils/SendingQueueV2.cs
@@ -4,7 +4,10 @@ using System.Threading;
 
 namespace SuperSocket.SocketBase
 {
-    public class SendingQueueV2 : IList<ArraySegment<byte>>
+    /// <summary>
+    /// SendingQueueV2
+    /// </summary>
+    public class SendingQueueV2 : ISendingQueue
     {
         private readonly int m_Offset;
 
@@ -18,6 +21,12 @@ namespace SuperSocket.SocketBase
 
         private static ArraySegment<byte> m_Null = default(ArraySegment<byte>);
 
+        /// <summary>
+        /// SendingQueueV2
+        /// </summary>
+        /// <param name="globalQueue"></param>
+        /// <param name="offset"></param>
+        /// <param name="capacity"></param>
         public SendingQueueV2(ArraySegment<byte>[] globalQueue, int offset, int capacity)
         {
             m_GlobalQueue = globalQueue;
@@ -42,29 +51,18 @@ namespace SuperSocket.SocketBase
             throw new NotSupportedException();
         }
 
-        ArraySegment<byte> IList<ArraySegment<byte>>.this[int index]
+        /// <summary>
+        /// Gets or sets the element at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotSupportedException"></exception>
+        public ArraySegment<byte> this[int index]
         {
             get
             {
                 var targetIndex = m_Offset + m_InnerOffset + index;
-                var value = m_GlobalQueue[targetIndex];
-
-                if (value.Array != null)
-                    return value;
-
-                var spinWait = new SpinWait();
-
-                while (true)
-                {
-                    spinWait.SpinOnce();
-                    value = m_GlobalQueue[targetIndex];
-
-                    if (value.Array != null)
-                        return value;
-
-                    if (spinWait.Count > 50)
-                        return value;
-                }
+                return m_GlobalQueue[targetIndex];
             }
             set
             {
@@ -76,12 +74,48 @@ namespace SuperSocket.SocketBase
 
         #region ICollection implementation
 
-        void ICollection<ArraySegment<byte>>.Add(ArraySegment<byte> item)
+        /// <summary>
+        /// add single one item into the queue
+        /// </summary>
+        /// <param name="item">the item to be inserted</param>
+        public void Add(ArraySegment<byte> item)
         {
             lock (this)
             {
                 if (m_CurrentCount == m_Capacity)
-                    return;
+                {
+                    throw new Exception("This sending queue is full, you cannot insert items into it any more.");
+                }
+
+                var oldCount = m_CurrentCount;
+                m_GlobalQueue[m_Offset + oldCount] = item;
+                m_CurrentCount = oldCount + 1;
+            }
+        }
+
+        /// <summary>
+        /// add multiple items into the queue
+        /// </summary>
+        /// <param name="itemsSource">the source cof the multiple items to be inserted</param>
+        public void AddRange(Func<IList<ArraySegment<byte>>> itemsSource)
+        {
+            lock (this)
+            {
+                var items = itemsSource();
+
+                if ((m_CurrentCount + items.Count) > m_Capacity)
+                {
+                    throw new Exception("This sending queue is going to be full, you cannot insert items into it.");
+                }
+
+                var oldCount = m_CurrentCount;
+
+                for (var i = 0; i < items.Count; i++)
+                {
+                    m_GlobalQueue[m_Offset + oldCount + i] = items[i];
+                }
+                
+                m_CurrentCount = oldCount + items.Count;
             }
         }
 
@@ -103,7 +137,13 @@ namespace SuperSocket.SocketBase
 
         void ICollection<ArraySegment<byte>>.CopyTo(ArraySegment<byte>[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            lock(this)
+            {
+                for (var i = 0; i < Count; i++)
+                {
+                    array[arrayIndex + i] = this[i];
+                }
+            }
         }
 
         bool ICollection<ArraySegment<byte>>.Remove(ArraySegment<byte> item)
@@ -123,7 +163,42 @@ namespace SuperSocket.SocketBase
         {
             get
             {
-                throw new NotImplementedException();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.
+        /// </summary>
+        /// <returns>The number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.</returns>
+        public int Count
+        {
+            get { return m_CurrentCount - m_InnerOffset; }
+        }
+
+        /// <summary>
+        /// Trim the internal segments at the begining by the binary data size.
+        /// </summary>
+        /// <param name="offset">The binary data size should be trimed at the begining.</param>
+        public void InternalTrim(int offset)
+        {
+            var innerCount = m_CurrentCount - m_InnerOffset;
+            var subTotal = 0;
+
+            for (var i = m_InnerOffset; i < innerCount; i++)
+            {
+                var segment = m_GlobalQueue[m_Offset + i];
+                subTotal += segment.Count;
+
+                if (subTotal <= offset)
+                    continue;
+
+                m_InnerOffset = i;
+
+                var rest = subTotal - offset;
+                m_GlobalQueue[m_Offset + i] = new ArraySegment<byte>(segment.Array, segment.Offset + segment.Count - rest, rest);
+
+                break;
             }
         }
 
