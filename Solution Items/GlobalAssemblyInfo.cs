55394e18129d6be11107d07103c69b3b1f9c4f1f
Kerry Jiang <kerry-jiang@hotmail.com>
2016-06-28 16:26:24 +0800

    upgraded SuperSocket.ProtoBase to .net core 1.0 and 1.7.0.7

--- Solution Items/GlobalAssemblyInfo.cs
+++ Solution Items/GlobalAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.0.6")]
-[assembly: AssemblyFileVersion("1.7.0.6")]
+[assembly: AssemblyVersion("1.7.0.7")]
+[assembly: AssemblyFileVersion("1.7.0.7")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket")]
 [assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2010-2016")]
