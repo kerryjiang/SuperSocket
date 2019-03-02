e31b096988747e4f0f90657b3bd6c130ce0f7ff8
Kerry Jiang <kerry-jiang@hotmail.com>
2019-03-02 15:05:34 -0800

    added test cases for FixedSize Protocol

---
+++ test/Test/FixedSizeProtocolTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using SuperSocket.ProtoBase;
+using SuperSocket.Server;
+using Xunit;
+
+namespace Tests
+{
+    [Collection("Protocol.FixedSize")]
+    public class FixedSizeProtocolTest : ProtocolTestBase
+    {
+        class MyFixedSizePiplelineFilter : FixedSizePipelineFilter<TextPackageInfo>
+        {
+            public MyFixedSizePiplelineFilter()
+                : base(36)
+            {
+
+            }
+
+            public override TextPackageInfo ResolvePackage(ReadOnlySequence<byte> buffer)
+            {
+                return new TextPackageInfo { Text = buffer.GetString(Encoding.UTF8) };
+            }
+        }
+
+        protected override string CreateRequest(string sourceLine)
+        {
+            return sourceLine;
+        }
+
+        protected override SuperSocketServer CreateSevrer()
+        {
+            return CreateSocketServer<TextPackageInfo, MyFixedSizePiplelineFilter>(packageHandler: async (s, p) =>
+            {
+                await s.Channel.SendAsync(new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(p.Text + "\r\n")));
+            });
+        }
+    }
+}
