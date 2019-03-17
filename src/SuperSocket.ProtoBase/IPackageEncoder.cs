27c28ed120b17f5f1c5f3c99c98b157bdc9e326f
Kerry Jiang <kerry-jiang@hotmail.com>
2019-03-16 18:59:19 -0700

    added PackageEncoder

--- src/SuperSocket.ProtoBase/IPackageEncoder.cs
+++ src/SuperSocket.ProtoBase/IPackageEncoder.cs
@@ -4,7 +4,6 @@ using System.IO.Pipelines;
 namespace SuperSocket.ProtoBase
 {
     public interface IPackageEncoder<in TPackageInfo>
-        where TPackageInfo : class
     {
         int Encode(PipeWriter writer, TPackageInfo pack);
     }
