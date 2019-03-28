157dfdcc75b97caa9a8b71a1f51bae8b3660b7f8
Kerry Jiang <kerry-jiang@hotmail.com>
2019-03-27 23:57:35 -0700

    designed interfaces for Command

---
+++ src/SuperSocket.Command/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SuperSocket.Command
+{
+    public interface ICommand<TKey, TPackageInfo>
+        where TPackageInfo : IKeyedPackageInfo<TKey>
+    {
+        TKey Key { get; }
+
+        string Name { get; }
+
+        bool IsAsync { get; }
+
+        void Execute(IAppSession session, TPackageInfo package);
+
+        Task ExecuteAsync(IAppSession session, TPackageInfo package);
+    }
+}
---
+++ src/SuperSocket.Command/IKeyedPackageInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SuperSocket.Command
+{
+    public class IKeyedPackageInfo<TKey>
+    {
+        public TKey Key { get; set; }
+    }
+}
