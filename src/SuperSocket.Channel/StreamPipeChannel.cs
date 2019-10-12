52adbc293947575e50b1b0215d3e1f547bc5d212
Kerry Jiang <kerry-jiang@hotmail.com>
2019-10-11 18:39:23 -0700

    added StreamPipeChannel

---
+++ src/SuperSocket.Channel/StreamPipeChannel.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipelines;
+using SuperSocket.ProtoBase;
+
+namespace SuperSocket.Channel
+{
+    public class StreamPipeChannel<TPackageInfo> : PipeChannel<TPackageInfo>
+        where TPackageInfo : class
+    {
+        private Stream _stream;
+
+        public StreamPipeChannel(Stream stream, IPipelineFilter<TPackageInfo> pipelineFilter, ChannelOptions options)
+            : base(pipelineFilter, options)
+        {
+            _stream = stream;
+        }
+
+        public override void Close()
+        {
+            _stream.Close();
+        }
+
+
+        protected override void OnClosed()
+        {
+            _stream = null;
+            base.OnClosed();
+        }
+
+        protected override async ValueTask<int> SendAsync(ReadOnlySequence<byte> buffer)
+        {
+            foreach (var piece in buffer)
+            {
+                await _stream.WriteAsync(piece);
+            }
+
+            return (int)buffer.Length;
+        }
+
+        protected override async ValueTask<int> FillPipeWithDataAsync(Memory<byte> memory)
+        {
+            return await _stream.ReadAsync(memory);
+        }
+    }
+}
