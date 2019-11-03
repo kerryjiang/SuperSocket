a99239314715d2501ce4ca28d6e604fbad3e4166
Kerry Jiang <kerry-jiang@hotmail.com>
2019-11-02 21:36:42 -0700

    implemented ToString() for TextPackageInfo

--- src/SuperSocket.ProtoBase/TextPackageInfo.cs
+++ src/SuperSocket.ProtoBase/TextPackageInfo.cs
@@ -3,5 +3,10 @@ namespace SuperSocket.ProtoBase
     public class TextPackageInfo
     {
         public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
     }
 }
