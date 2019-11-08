a6268d312adc0e719d956a63afa418dbe64bf7f8
Kerry Jiang <kerry-jiang@hotmail.com>
2019-11-07 23:46:49 -0800

    load text message

--- src/SuperSocket.WebSocket/FramePartReader/PayloadDataReader.cs
+++ src/SuperSocket.WebSocket/FramePartReader/PayloadDataReader.cs
@@ -3,6 +3,7 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SuperSocket.ProtoBase;
 
 namespace SuperSocket.WebSocket.FramePartReader
 {
@@ -21,7 +22,14 @@ namespace SuperSocket.WebSocket.FramePartReader
             }
 
             needMoreData = false;
-            package.Data = reader.Sequence.Slice(0, required);
+
+            var seq = reader.Sequence.Slice(0, required);
+
+            if (package.OpCode == OpCode.Binary)
+                package.Data = seq;
+            else
+                package.Message = seq.GetString(Encoding.UTF8);
+
             reader.Advance(required);
             
             return true;
