a1ffe46df94df97d54d07fa64cec7a5749d740c6
Kerry Jiang <kerry-jiang@hotmail.com>
2019-12-29 09:58:53 -0700

    added a sample for websocket server

---
+++ samples/WebSocketServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SuperSocket.WebSocket.Server;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {               
+            var host = WebSocketHostBuilder.Create()
+                .ConfigureWebSocketMessageHandler(async (session, message) =>
+                {
+                    // echo message back to the client
+                    await session.SendAsync(message.Message);
+                })
+                .ConfigureLogging((hostCtx, loggingBuilder) =>
+                {
+                    // register your logging library here
+                    loggingBuilder.AddConsole();
+                }).Build();
+
+            await host.RunAsync();
+        }
+    }
+}
