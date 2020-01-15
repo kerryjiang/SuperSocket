00f0ac5d0c7789e4e0afe50b7e28cd837dd011d8
Kerry Jiang <kerry-jiang@hotmail.com>
2020-01-14 22:06:57 -0800

    allowed CommandAttribute's Key can be declared independent

--- src/SuperSocket.Command/CommandAttribute.cs
+++ src/SuperSocket.Command/CommandAttribute.cs
@@ -5,11 +5,11 @@ namespace SuperSocket.Command
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class CommandAttribute : Attribute
     {
-        public string Name { get; private set; }
+        public string Name { get; set; }
 
-        public object Key { get; private set; }
+        public object Key { get; set; }
 
-        public CommandAttribute(string name = null, object key = null)
+        public CommandAttribute(string name, object key)
         {
             Name = name;
             Key = key;
