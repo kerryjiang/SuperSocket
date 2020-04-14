c4fde2cd98c7c540cfd2067532afa10cd300a192
Kerry Jiang <kerry-jiang@hotmail.com>
2020-04-13 18:50:55 -0700

    allowed different encoding for DefaultStringPackageDecoder

--- src/SuperSocket.Primitives/DefaultStringPackageDecoder.cs
+++ src/SuperSocket.Primitives/DefaultStringPackageDecoder.cs
@@ -8,9 +8,22 @@ namespace SuperSocket
 {
     public class DefaultStringPackageDecoder : IPackageDecoder<StringPackageInfo>
     {
+        public Encoding Encoding { get; private set; }
+
+        public DefaultStringPackageDecoder()
+            : this(new UTF8Encoding(false))
+        {
+
+        }
+
+        public DefaultStringPackageDecoder(Encoding encoding)
+        {
+            Encoding = encoding;
+        }
+
         public StringPackageInfo Decode(ReadOnlySequence<byte> buffer, object context)
         {
-            var text = buffer.GetString(Encoding.UTF8);
+            var text = buffer.GetString(Encoding);
             var parts = text.Split(' ');
 
             return new StringPackageInfo
