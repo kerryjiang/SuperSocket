4b844f32fd9cac9086be469c53ab909f23c7ef2f
Kerry Jiang <kerry-jiang@hotmail.com>
2020-08-11 21:33:39 -0700

    added benchmark project

---
+++ test/SuperSocket.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace SuperSocket.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly, new BenchmarkConfig());
+        }
+    }
+}
