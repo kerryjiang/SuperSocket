620336f6fa8a796be07e3e05fe00e1c5fb4f5865
Kerry Jiang <kerry-jiang@hotmail.com>
2021-01-08 11:13:48 -0800

    fixed the stackoverflow in the support of udp enableSendingPipe

--- src/SuperSocket.Channel/UdpPipeChannel.cs
+++ src/SuperSocket.Channel/UdpPipeChannel.cs
@@ -95,7 +95,7 @@ namespace SuperSocket.Channel
         public override async ValueTask SendAsync(ReadOnlyMemory<byte> buffer)
         {
             if (_enableSendingPipe)
-                await SendAsync(buffer);
+                await base.SendAsync(buffer);
 
             await SendOverIOAsync(new ReadOnlySequence<byte>(buffer), CancellationToken.None);
         }
@@ -103,7 +103,7 @@ namespace SuperSocket.Channel
         public override async ValueTask SendAsync<TPackage>(IPackageEncoder<TPackage> packageEncoder, TPackage package)
         {
             if (_enableSendingPipe)
-                await SendAsync(packageEncoder, package);
+                await base.SendAsync(packageEncoder, package);
 
             try
             {
@@ -122,7 +122,7 @@ namespace SuperSocket.Channel
         public override async ValueTask SendAsync(Action<PipeWriter> write)
         {
             if (_enableSendingPipe)
-                await SendAsync(write);
+                await base.SendAsync(write);
 
             throw new NotSupportedException($"The method SendAsync(Action<PipeWriter> write) cannot be used when noSendingPipe is true.");
         }
