c4571acf0fc5494a4f17211a1bcb8047b7cbb4eb
yedajiang44 <602830483@qq.com>
2021-01-29 16:03:27 +0800

    add ISessionContainer use in asp net core

---
+++ samples/AspNetSample/Controllers/SessionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperSocket;
+using System.Linq;
+using System.Text;
+
+namespace AspNetSample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SessionController : ControllerBase
+    {
+        readonly ISessionContainer sessionContainer;
+        public SessionController(ISessionContainer sessionContainer)
+        {
+            this.sessionContainer = sessionContainer;
+        }
+        [HttpGet]
+        public IActionResult IndexAsync()
+        {
+            return Ok(sessionContainer.GetSessions());
+        }
+        public IActionResult BroadcastMessage(string message)
+        {
+            try
+            {
+                sessionContainer.GetSessions().ToList().ForEach(async x => await x.SendAsync(Encoding.UTF8.GetBytes(message)));
+            }
+            catch
+            {
+                return new StatusCodeResult(500);
+            }
+            return Ok();
+        }
+    }
+}
--- samples/AspNetSample/Program.cs
+++ samples/AspNetSample/Program.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SuperSocket;
 using SuperSocket.ProtoBase;
 
@@ -21,14 +15,16 @@ namespace AspNetSample
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                }).AsSuperSocketHostBuilder<TextPackageInfo, LinePipelineFilter>()
-                .UsePackageHandler(async (s, p) =>
-                {
-                    // echo message back to client
-                    await s.SendAsync(Encoding.UTF8.GetBytes(p.Text + "\r\n"));
-                });
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .AsSuperSocketHostBuilder<TextPackageInfo, LinePipelineFilter>()
+            .UsePackageHandler(async (s, p) =>
+            {
+                // echo message back to client
+                await s.SendAsync(Encoding.UTF8.GetBytes(p.Text + "\r\n"));
+            })
+            .UseInProcSessionContainer();
     }
 }
