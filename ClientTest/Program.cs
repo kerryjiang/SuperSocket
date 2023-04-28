1b06cafcc57c913bde832a11fb8225b01bdbc3c0
吴俊 <8400684@qq.com>
2023-04-29 01:20:23 +0800

    添加websocket出现bug程序集

---
+++ ClientTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Buffers;
+using System.Net.WebSockets;
+using System.Text;
+
+var client = new ClientWebSocket();
+
+await client.ConnectAsync(new Uri("ws://127.0.0.1:4040"), CancellationToken.None);
+
+var sendData = Encoding.UTF8.GetBytes(ClientTest.Properties.Resources.大json_2_);
+
+Console.WriteLine($"发送数据：{sendData.Length}");
+
+await client.SendAsync(
+    endOfMessage: true,
+    buffer: new ArraySegment<byte>(sendData),
+    messageType: WebSocketMessageType.Text,//只有Text模式会出现
+    cancellationToken: CancellationToken.None);
+
+Console.WriteLine($"正在接受...");
+
+while (true)
+{
+    var receiveBuffer = ArrayPool<byte>.Shared.Rent(sendData.Length);
+
+    try
+    {
+        var webSocketReceiveResult = await client.ReceiveAsync(receiveBuffer.AsMemory(), CancellationToken.None);
+
+        var result = Encoding.UTF8.GetString(receiveBuffer, 0, webSocketReceiveResult.Count);
+
+        Console.WriteLine($"接受长度：{webSocketReceiveResult.Count}");
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine(ex);
+        break;
+    }
+    finally
+    {
+        ArrayPool<byte>.Shared.Return(receiveBuffer);
+    }
+}
+
+Console.ReadKey();
---
+++ WebSocketServerBugTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SuperSocket.WebSocket;
+using SuperSocket.WebSocket.Server;
+
+var host = WebSocketHostBuilder.Create()
+    .UseWebSocketMessageHandler
+    (
+        async (session, message) =>
+        {
+            var result = message.OpCode switch
+            {
+                OpCode.Text => message.Message.Length,
+                OpCode.Binary => message.Data.Length,
+                _ => 0,
+            };
+
+            Console.WriteLine($"消息类型：{message.OpCode},消息长度：{result}");
+
+            await session.SendAsync(message);
+        }
+    )
+    .ConfigureAppConfiguration((hostCtx, configApp) =>
+    {
+        configApp.AddInMemoryCollection(new Dictionary<string, string>
+        {
+            { "serverOptions:name", "TestServer" },
+            { "serverOptions:listeners:0:ip", "Any" },
+            { "serverOptions:listeners:0:port", "4040" }
+        });
+    })
+    .ConfigureLogging((hostCtx, loggingBuilder) =>
+    {
+        loggingBuilder.AddConsole();
+    })
+    .Build();
+
+await host.RunAsync();
