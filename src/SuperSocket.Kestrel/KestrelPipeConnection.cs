ad4d06430514ce056e33f657790599c329fde55b
mockingjay <8400684@qq.com>
2024-04-23 11:33:09 +0800

    add connectionToken property from IConnection

--- src/SuperSocket.Kestrel/KestrelPipeConnection.cs
+++ src/SuperSocket.Kestrel/KestrelPipeConnection.cs
@@ -16,7 +16,7 @@ public class KestrelPipeConnection : PipeConnectionBase
         : base(context.Transport.Input, context.Transport.Output, options)
     {
         _context = context;
-        context.ConnectionClosed.Register(() => OnClosed());
+        context.ConnectionClosed.Register(() => OnConnectionClosed());
         LocalEndPoint = context.LocalEndPoint;
         RemoteEndPoint = context.RemoteEndPoint;
     }
@@ -72,5 +72,11 @@ public class KestrelPipeConnection : PipeConnectionBase
     {
         await base.SendAsync(packageEncoder, package, cancellationToken);
         UpdateLastActiveTime();
+    }
+
+    private void OnConnectionClosed()
+    {
+        OnClosed();
+        Cancel();
     }
 }
