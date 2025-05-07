5b0e5db7c6c52b7fc5b107887b7b450f4874d8fe
hyabean <hongyuan.jiang@outlook.com>
2025-05-07 08:38:41 +0800

    失败的单元测试

--- test/SuperSocket.Tests/WebSocket/WebSocketBasicTest.cs
+++ test/SuperSocket.Tests/WebSocket/WebSocketBasicTest.cs
@@ -245,7 +245,7 @@ namespace SuperSocket.Tests.WebSocket
                 .UseInProcSessionContainer()
                 .BuildAsServer())
             {
-                Assert.True(await server.StartAsync());
+                Assert.True(await server.StartAsync(TestContext.Current.CancellationToken));
                 OutputHelper.WriteLine("Server started.");
 
                 var sessionContainer = server.GetSessionContainer();
@@ -269,16 +269,20 @@ namespace SuperSocket.Tests.WebSocket
 
                     Assert.Equal(WebSocketState.Closed, websocket.State);
                 });
-                
+
+                Assert.True(0 == server.SessionCount, $"server.SessionCount: {server.SessionCount}");
                 Assert.Equal(0, server.SessionCount);
+                Assert.True(0 == sessionContainer.GetSessionCount(), $"sessionContainer.GetSessionCount(): {sessionContainer.GetSessionCount()}");
                 Assert.Equal(0, sessionContainer.GetSessionCount());
 
-                await Task.Delay(1000);
+                await Task.Delay(1000, CancellationToken.None);
 
+                Assert.True(0 == websocketMiddleware.OpenHandshakePendingQueueLength, $"websocketMiddleware.OpenHandshakePendingQueueLength: {websocketMiddleware.OpenHandshakePendingQueueLength}");
                 Assert.Equal(0, websocketMiddleware.OpenHandshakePendingQueueLength);
+                Assert.True(0 == websocketMiddleware.CloseHandshakePendingQueueLength, $"websocketMiddleware.CloseHandshakePendingQueueLength: {websocketMiddleware.CloseHandshakePendingQueueLength}");
                 Assert.Equal(0, websocketMiddleware.CloseHandshakePendingQueueLength);
 
-                await server.StopAsync();
+                await server.StopAsync(TestContext.Current.CancellationToken);
             }
         }
 
